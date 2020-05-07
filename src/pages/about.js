8e3c40e72339aab600490d0293f98b40f4b20f3d
Vaishak Kumar <vaishak.thimakapura@gmail.com>
2020-05-07 16:32:55 -0700

    cheshta corrections about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,12 +6,12 @@ export default () => <div>
         <div>
             <h3>Feeding the World while keeping it intact</h3>
             <p>Farmers face the "Agricultural Imperative" - the challenge of feeding 9 Billion people by the year 2050.
-               This is no easy task. The current methods will not suffice. Nor will romantic approaches that hark back to the pre-Green Revolution era.
+               This is no easy task. The current methods will not suffice. Nor will romantic approaches that harken back to the pre-Green Revolution era.
                This challenge requires us to reduce the environmental footprint of industrial agriculture while increasing the efficiency of small landholder farms.
             </p>
 
             <h3>Information is the key</h3>
-            <p>Age old practices passed from generation to generation in remote parts of world hold promise just as the latest genetic tools in labs.
+            <p>Age old practices passed from generation to generation in remote parts of world hold promise, as do the latest genetic tools in labs.
                Each context is different and farmers need to be exposed to as many tools as possible so that they can pick the ones that work for them.</p>
             
             <h3>Nesara</h3>
@@ -21,23 +21,23 @@ export default () => <div>
             </p>
 
             <p>
-                I started Nesara right after I graduated from college. It started out as a "helpline" that used Whatsapp to connect farmers in rural India with crop disease specialists. 
-                Farmers could send photos of their diseased crops to the Whatsapp number that we would show to specialists, and then reply back with a diagnosis and treatment measures. 
+                I started Nesara right after I graduated from college. It started out as a "helpline" that used WhatsApp to connect farmers in rural India with crop disease specialists. 
+                Farmers could send us photos of their diseased crops that we would show to specialists, and then reply back with a diagnosis and treatment measures. 
                 I was very young then and, owing to my social sciences training, saw agriculture mostly as an economic sector that housed the most number of workers. 
-                I saw Nesara as an opportunity to help farmers at the brink - those who were at dire straits due to pests, diseases or some other form of crop damage. 
-                While our efforts were useful to farmers, I began to see it as a fool's errand to try and solve the problems of farmers by diagnosing plant diseases. 
+                I saw Nesara as an opportunity to help farmers at the brink - those who were in dire straits due to pests, diseases or some other form of crop damage. 
+                While our efforts were useful to farmers, I began to see it as a fool's errand to try and solve the problems of farmers just by diagnosing plant diseases. 
             </p>
             <p>
-                Through massive surveys that I and my friend conducted in villages in the Mysore District, we learnt that diseases were only a small part of the "agriculture problem."
+                Through massive surveys that we conducted in villages in the Mysore District, we learnt that diseases were only a small part of the "agriculture problem".
                 Farmers were using chemicals indiscriminately, not properly fertilizing their fields, not following proper crop rotation practices etc. Their productivity was low not 
-                because of just pests and pathogens. Their productivity was low because almost everything that they were doing was wrong. The new generation of farmers were stuck in a weird 
-                limbo between following what had been done on their land for centuries or even millenia, and the new "Green Revolution" practices introduced starting in the 1960s. 
-                Education is required to combine the ancient practices and new technologies into an integrated system that works for each farmer's particular context. 
+                just because of pests and pathogens. Their productivity was low because almost everything that they were doing was suboptimal. The new generation of farmers were stuck in a weird 
+                limbo between following what had been done on their land for centuries or even millenia, and the new Green Revolution practices introduced starting in the 1960s. 
+                While both have great value, education is required to combine the ancient practices and new technologies into an integrated system that works for each farmer's particular context. 
             </p>
             <p>
-                Nesara's mission is to assist farmers at every step of the crop cycle. Instead of just helping "cure" diseases, we decided that we would educate 
+                Nesara's mission is to assist farmers at every step of the crop cycle. Instead of just helping treat diseases, we aim to educate 
                 farmers about plants, practices, and products that would help boost their productivity while making sure that their land and environment are sustainably managed. 
-                To achieve that we are compiling the world's most comprehensive guide to farming. This is not a project that has an end. Farming began 10,000 years ago. It is still evolving.
+                We are working with farmers and scientists from across the world to compile the world's most comprehensive guide to farming. This is not a project that has an end. Farming began 10,000 years ago. It is still evolving.
             </p>
 
             <h2>We are still learning. We are still teaching. </h2>
