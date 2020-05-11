b793034987306966664fcac1f9cd3058d0149ddd
Mona Lisa <cheshta@sas.upenn.edu>
2020-05-10 19:24:51 -0700

    added soil types

--- src/pages/soil.js
+++ src/pages/soil.js
@@ -17,5 +17,50 @@ export default () => <Layout>
 <p>Soils need to be scientifically managed forsustained crop production over long periods of time.</p>
 <p>Soil properties decide the nature of conversion, conservation of nutrients, and how they are made available to the plants.</p>
 
+<strong>Soil Types</strong>	
+<p><em>1. Alluvial soils (Entisols)</em></p>
+<p><ul>
+    <li>These are most important from agriculture point of view.</li>
+    <li>Alluvial soils are formed mainly due to silt deposited by rivers.</li>
+    <li>In coastal regions, some alluvial deposits are formed due to wave action.</li>
+    <li>The parent material of these soils is of transported origin.</li>
+    <li>The soil is porous because it has about equal proportions of sand and clay (loamy), but clayey soils are not uncommon.</li>
+    <li>While they are generally low in Nitrogen, the proportion of Potash, Phosphoric Acid and alkalies are adequate.</li>
+    <li>They are best suited to irrigation and respond well to canal and well/tube-well irrigation.</li>
+</ul></p>
+
+<p><em>2. Black soils (Vertisols)</em></p>
+<p><ul>
+    <li>A typical black soil has a high clay content of 62% or more.</li>
+    <li>The black soils have high moisture retention capacity, and are generally neutral to slightly alkaline.</li>
+    <li>When dry, soil shrinks and develops deep cracks permitting oxygenation of the soil to sufficient depths and thus enhancing fertility.</li>
+    <li>The black colour is due to the presence of a small proportion of Titaniferous Magnetite or Iron and black constituents of the parent rock.</li>
+    <li>They generally contain 10% Alumina, 9-10% Iron Oxide, 6-8% lime and Magnesium Carbonates, less than 0.5% Potash and are low in Phosphates, Nitrogen and humus.</li>
+    <li>This soil has been used for growing a variety of crops for centuries without adding fertilizers and manures.</li>
+</ul></p>
+
+<p><em>3. Red soils (Alfisols)</em></p>
+<p><ul>
+    <li>The main parent rocks are crystalline and metamorphic rocks.</li>
+    <li>The red colour is due to the presence of oxides of Iron.</li>
+    <li>The texture of these soils can vary from sand to clay, the majority being loams.</li>
+    <li>On the uplands, the red soils are poor, gravelly and porous.</li>
+    <li>But in the lower areas they are rich, deep dark and fertile.</li>
+    <li>They are mostly acidic and are poor in lime, Magnesium, Phosphates, Nitrogen and humus.</li>
+    <li>They are fairly rich in Potassium.</li>
+    <li>The red soils are mostly loamy and hence cannot retain water unlike the black soils.</li>
+</ul></p>
+
+<p><em>4. Laterite soils (Oxisols)</em></p>
+<p><ul>
+    <li>Laterite soils are mostly the end products of weathering, formed under high temperatures and high rainfall with alternate wet and dry periods.</li>
+    <li>Laterite soils are red in color due to little clay and more gravel of red sand-stones.</li>
+    <li>Lime and Silica are leached away in heavy rainfall, but the soil is rich in oxides of Iron and Aluminium compounds.</li>
+    <li>They are very poor in lime, Magnesium, Potash and Nitrogen.</li>
+    <li>Sometimes, the Phosphate content may be high in the form of Iron Phosphate.</li>
+    <li>Laterite soils lack fertility due to intensive leaching.</li>
+    <li>When manured and irrigated, some laterites are suitable for growing plantation crops like Tea, Coffee, Rubber, Cinchona, Coconut, Arecanut, etc.</li>
+</ul></p>
+
     </Layout>
 
