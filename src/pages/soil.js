bc040e033e3fa7d060dd0ca9fb9c8eacc3311e65
Mona Lisa <cheshta@sas.upenn.edu>
2020-05-13 22:36:52 -0700

    added Soil Composition, Soil properties

--- src/pages/soil.js
+++ src/pages/soil.js
@@ -17,7 +17,7 @@ export default () => <Layout>
 <p>Soils need to be scientifically managed forsustained crop production over long periods of time.</p>
 <p>Soil properties decide the nature of conversion, conservation of nutrients, and how they are made available to the plants.</p>
 
-<strong>Soil Types</strong>	
+<p><strong>Soil Types</strong></p>
 <p><em>1. Alluvial soils (Entisols)</em></p>
 <p><ul>
     <li>These are most important from agriculture point of view.</li>
@@ -100,8 +100,95 @@ export default () => <Layout>
     <li>When properly drained and fertilized, these soils produce good crops of rice.</li>
 </ul></p>
 
+<p><strong>SOIL COMPOSITION</strong></p>
+
+<p>An ideal soil is made of mineral matter (45-50%), organic matter (1-5%), air (25%) and water (25%).</p>
+
+<p><em>1. Soil Mineral Matter</em></p>
+<p><ul>
+    <li>Mineral matter is largely made up of Oxygen and Silicon.</li>
+    <li>Most abundantly available plant nutrients are Iron, Potassium and Magnesium.</li>
+    <li>The particulate size ranges from large pieces of gravel to coarse sand (2 - 0.2 mm) to fine clay (0.002 mm).</li>
+    <li>The mineral matter determines the soil texture and its water holding capacity.</li>
+    <li>Silt is a storehouse of nutrients, while clay along with organic matter is most active.</li>
+
+</ul></p>
+
+<p><em>2. Soil Organic Matter</em></p>
+<p><ul>
+    <li>The organic matter (OM) consists of a variety of (Bacteria, Fungi, Algae, Actinomycetes, etc.), animal life (Earthworms, Nematodes, Protozoa, Mites, Beetles, Moles, etc.), their excretions, debris of dead bodies decomposed to various extents.</li>
+    <li>It ranges from less than 1% in many soils in the plains to 3-4% in forest and mountain soils.</li>
+    <li>Average composition of OM is 47% C, 44% O, 7% H, 2% N and traces of other elements.</li>
+    <li>It is the storehouse of nutrients, affects cation exchange, water retention, and fixation and release of nutrients.</li>
+</ul></p>
+
+<p><em>3. Soil Water and Air</em></p>
+<p><ul>
+    <li>Soil water that is also called Soil Solution, is the dilute solution containing small amounts of every soluble compound present in the soils.</li>
+    <li>Soil water promotes physical and biological activities of soil, acts as solvent and carrier of nutrients and maintains turgidity of plants.</li>
+    <li>Soil water transports nutrients to the root, to the soil surface or subsoil depending on the direction of water flow.</li>
+    <li>Soil air is essential for most of the biological reactions in the soil.</li>
+    <li>Soil air has about ten times more carbon dioxide than atmospheric air, due to respiration by roots and soil microorganisms.</li>
+    <li>Soil air is the pathway for intake of oxygen that is absorbed by soil microorganisms and plant roots and for escape of Carbon dioxide produced by the plants.</li>
+    <li>This process called soil aeration is critical for plant growth when water content is high and water replaces soil air.</li>
+
+</ul></p>
+
+<p><em>Major role of Organic matter</em></p>
+<p><ul>
+    <li>Binds different soil particles.</li>
+    <li>Increases rate of infiltration.</li>
+    <li>Increases Water Holding Capacity (WHC).</li>
+    <li>Increases ability to resist erosion.</li>
+    <li>Acts as mulch.</li>
+    <li>Reduces alkalinity.</li>
+    <li>Increases Cation Exchange Capacity (CEC).</li>
+    <li>Solubilizing effect on certain minerals.</li>
+    <li>Store house of nutrients.</li>
+    <li>Temporarily absorbs heavy metal pollutants.</li>
+    <li>Source of energy for micro-organisms.</li>
+    <li>Acts as chelate.</li>
+    <li>Improves soil aeration.</li>
+
+</ul></p>
 
 
+<p><strong>SOIL PROPERTIES</strong></p>
+
+<p><em>Soil Texture</em></p>
+<p><ul>
+
+    <li>Three categories of soil particles - sand, silt and clay are established based on their particle size.</li>
+    <li>Clay particles are the smallest (<0.002 mm), while sand particles are the largest (0.05- 2.00 mm).</li>
+    <li>Soil texture depends on the proportion of sand, silt and clay.</li>
+    <li>There are 12 classes of soil texture.</li>
+    <li>If most particles are large and coarse the soil is called Sand.</li>
+    <li>Silt is dominated by medium-sized particles and feels like flour.</li>
+    <li>Small-sized soil particles primarily make up a Clay soil.</li>
+
+</ul></p>
+
+<p><em>Soil water relations</em></p>
+<p><ul>
+    <li>Soil texture and organic matter content determine its water holding capacity (WHC), rate of water intake, percolation, Cation Exchange Capacity (CEC), etc.</li>
+    <li>WHC and CEC increase with increase in clay content and depth.</li>
+    <li>Water intake and percolation increase with increase in the proportion of sand.</li>
+</ul></p>
+
+<p><em>Field Capacity:</em></p>
+<p>Maximum amount of water a soil holds after free drainage, e.g. 10% in sandy soils, 34% in loamy soils and 40% in clay soils.</p>
+
+<p><em>Wilting point:</em></p>
+<p>The amount of available water in the soil, below which plants start to wilt, e.g. 4% in sandy soils, 16% in loamy soils and 24% in clay soils.</p>
+
+<p><em>Available water:</em></p>
+<p>Water held by soil that is available for crop production i.e. FC - WP. e.g.. 6% in sandy soils, 18 % in loamy soils and 16% in clay soils.</p>
+
+<p><em>Soil pH</em></p>
+<p><ul>
+    <li>Soil pH is a very important character that indicates whether the soil is acidic, alkaline or neutral.</li>
+    <li>It is the negative logarithm of the hydrogen ion (H+) activity and is measured on a 0-14 unit less scale.</li>
+</ul></p>
     </Layout>
 
 
