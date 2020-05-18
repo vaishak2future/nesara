fa53ed84f5d0d2ac869ab4ae297ab3ba4dd8fc87
Vaishak Kumar <vaishak.thimakapura@gmail.com>
2020-05-18 10:20:57 -0700

    continued fungal diseases

--- src/pages/diseases/fungal-diseases.js
+++ src/pages/diseases/fungal-diseases.js
@@ -124,6 +124,7 @@ durations, either in the soil or in plant debris.
 groups differing from each other in respect
 of the mode of survival when they are not
 parasitically active in their host plants. </p>
+
 <h5>Group-1: Obligate parasites or biotrophs</h5>
 <p>•	 These are highly specialized parasites, which
 include pathogens like white rusts, downy
@@ -140,4 +141,287 @@ germination.
 •	 This inoculum is carried to the susceptible
 host, which it infects and begins to grow and
 multiply.</p>
+
+<h5>Group-2: Nectotrophs</h5>
+<p>•	 This group includes unspecialized parasites
+represented by a range of parasites with a
+wide host range.
+•	 Unlike the highly specialized fungi, theses
+fungal pathogens are necrotrophs and are
+able to live as saprophytes in the soil.
+•	 They grow parasitically on the host plants,
+continue to live, grow and multiply on the
+dead tissues of the host after its death and
+may even further move out of the host debris
+into the soil or other decaying plant materials
+on which they grow and multiply as strict
+saprophytes.
+•	 These are potential root infecting soil fungi.
+•	 In most cases, the dead plant matter
+colonized by them need not be related to the
+host parasitized by them.
+•	 However, some like Gaeumannomyces
+graminis are able to survive in soil only
+as long as their host residue persists as a
+substrate for their saprophytic existence.
+•	 They are also referred to as facultative
+saprophytes.
+•	 Other root-infecting fungi in this group
+include Pythium spp., Fusarium spp,
+Verticillium spp., Macrophomina phaseolina
+and many others, which are referred to as
+soil inhabitants.
+•	 They can live in soil as saprophytes for 5 to
+6 years in absence of their hosts.</p>
+
+<h5>Group-3: Intermediate parasites</h5>
+<p>•	 This group consists of intermediate parasites,
+which pass part of their cycles on the host
+as parasites and part on dead tissues of the
+same host on the ground as saprophytes in
+order to complete their life cycle in nature.
+•	 These are also necrotrophs.
+•	 The group includes pathogens like Venturia
+inaequalis (apple scab fungus), Taphrina
+deformans (peach leaf curl fungus) and
+Claviceps purpurea (ergot of cereals).
+•	 They cannot survive freely as saprophytes in
+soil.
+•	 Unlike the soil invaders which kill their host
+tissues in advance, they invade and kill their
+host tissues in order to complete a part of
+their life cycle.
+•	 They also do not grow and multiply on dead
+tissues of their hosts, as the soil invaders do,
+but they simply complete a part of their life
+cycle on dead host tissues.</p>
+
+<h5>Dissemination and spread:</h5>
+<p>•	 In zoosporic fungi (Chytridiomycetes) and
+FLOs (Oomycetes) zoospores are the means
+of dissemination of primary inoculum and
+spread of the disease from plant to plant in
+the field.
+•	 The zoospores can move for only short
+distances.
+•	 Majority of the fungal pathogens, however
+depend for their spread from plant to plant
+and to different parts of the same plant on
+agents such as wind, water, rain splash, birds,
+insects and other animals, and humans.
+•	 Among the agents of dissemination, wind
+perhaps is the most important for spores of
+most fungi and it carries spores over great
+distances.
+•	 They are disseminated primarily in the
+form of enormous variety of their spores,
+produced in different ways in different kinds
+of fruiting structures.
+•	 Spore dissemination in fungi is mostly
+passive, though initial discharge of spores in
+some fungi is forcible.
+•	 In specific cases, water or insects may play
+a much important role than wind in spore
+dissemination.</p>
+<h5>Sexual Spores</h5>
+<p>•	 Produced once at the end of the life cycle
+of the host.
+•	 Produced in smaller numbers, thick walled
+and long live.
+•	 Overwinter - ensure the survival of the
+species by virtue of their capacity to tide
+over the unfavorable seasons.
+•	 Products of meiosis.
+•	 Play a major role in survival and evolution
+of the species.</p>
+
+<h5>Asexual spores</h5>
+<p>•	 Produced many times within the crop cycle
+of the host.
+•	 Produced in enormous quantities, light and
+short lived.
+•	 Do not overwinter - no role in continuity of
+species.
+•	 Not products of meiosis and hence no role
+in evolution of a species.
+•	 Play a major role in spread of the disease.</p>
+
+<p>•	 In addition to sexual spores, scelorotia and other resting spores (chlamydospores, etc.) are also
+produced to overwinter or oversummer under adverse conditions.
+•	 These kind of spores are like a match sticks that can store the fire.
+•	 The asexual spores are like the many torches that a matchstick can ignite to spread the fire.
+•	 They are produced in abundance under favorable conditions, but very short-lived.
+•	 They are produced in many cycles during the crop season and their role is only to spread the
+disease in a short span of time.</p>
+
+<h4>ANTHRACNOSE CAUSED BY COLLETOTRICHUM SPP.</h4>
+<p>Hosts: Hot and Sweet Pepper, Beans, Black pepper, Cucurbits, Tobacco, Grapes, Banana, etc.
+Causal Organisms: Colletotrichum gloeosporioides, C. capsici, C. lindemuthianum,
+C. orbiculare, C. musae, C. tabacum, Elsinoe ampelina.</p>
+
+<h5>Symptoms</h5>
+<p>•	 Anthracnose is a group of diseases that
+appear as dark, sunken lesions on leaves,
+stems, flowers and fruits.
+•	 Colletotrichum leaf spots are very common
+in plant nurseries.
+•	 The fungus infects tender leaves, mostly at
+the leaf tip region.
+•	 Spots are small, brown in color and are
+surrounded by a yellow halo.
+•	 Later the small circular spots coalesce to form
+large elliptical spots on fruits and leaves.
+•	 Under severe conditions, defoliation of
+affected plants occurs.
+•	 The disease is characterized by theappearance of small white/brown/black
+circular spots on the leaves and skin of the
+fruits that spread in the direction of the long
+axis, thus becoming more or less elliptical.
+•	 As the infection progresses, the spots get
+either diffused and black, greenish or dirty
+grey in color or they are markedly delimited
+by a thick and sharp black outline enclosing
+a lighter black colored area.
+•	 The disease has been observed to occur in
+three phases, they are:
+ i. Seedling blight or Damping-off,
+ ii. Leaf spot and die back, and
+ iii. Anthracnose or fruit rot</p>
+
+ <h5>Pathogen Morphology</h5>
+ <p>•	 There are many species of Colletotrichum,
+which cause anthracnose in many important
+vegetable and horticultural crops.
+•	 Colletotrichum gloeosporioides forms onecelled, hyaline, cylindrical conidia, with
+obtuse ends, measuring 2.7 - 5.0 x 11.1 -
+18.5 µm.
+•	 It rarely produces setae.
+•	 Colletotrichum capsici forms sickle-shaped
+conidia, which measure 2.7 - 4.8 x 11.5 -
+25.0µm.
+•	 This species does not form sclerotia, but
+produces abundant setae.</p>
+
+<h5>Management</h5>
+<p>•	 Use disease free seeds as this is a highly
+seed-borne and seed transmitted disease.
+•	 Treat seeds with Carbendazim 50% WP (2
+- 3 gm/kg seed).
+•	 Remove and destroy diseased plant parts to
+check the spread of the disease.
+•	 Avoid sprinkler irrigation.
+•	 Instead use drip irrigation as it helps in
+restricting spread of the disease.
+•	 Improve drainage in the field and keep it free
+from infected plant debris.
+•	 Spray any of the systemic fungicides like
+o Carbendazim 50% WP (Bavistin/
+GLSTIN) @1 gm/ltr. or
+o Thiophanate Methyl 70% WP (Roak/
+Spring/Topsin M) @1 gm/ltr. or
+o Propiconazole 25% EC (Tilt) @ 1 ml/
+ltr. or
+o Difenoconazole 25% EC (Score) at 1
+ml/ltr. or
+•	 The following broad spectrum contact fungicides
+may also be sprayed
+o Chlorothalonil 75% WP (Kavach/
+Jatayu) @ 2 gm/ltr.
+o Mancozeb 75% WP (Dithane M-45) @
+2 gm/ltr.
+•	 Repeat the spray 2 - 3 times at 7 - 8 days
+intervals to completely control the disease.
+•	 Change the fungicides and spray if the
+problem persists.</p>
+
+<h5>BLAST DISEASE</h5>
+<p>Hosts: Rice, Ragi and Pearl millet
+Causal organism: Pyricularia oryzae (Rice), P. grisea (Ragi and Pearl millet)</p>
+<h5>Symptoms</h5>
+<p>•	 Mainly a leaf disease and typical symptoms
+appear on leaves, leaf sheath, rachis, nodes
+and glumes.
+•	 The lesions on leaves appear as small,
+bluish, water-soaked flecks of about 1 - 3
+mm in diameter.
+•	 These lesions rapidly enlarge, become several
+centimeters broad and spindle shaped.
+•	 The center of the spots appears pale green
+or dull greyish green changing to grey, and
+the periphery has a dark brown band with a
+yellow halo around the lesion.
+•	 Lesions may kill the entire leaf in susceptible
+varieties, but they remain as minute, pinhead sized brown specks in resistant one.
+•	 Infection of other parts of the shoot can
+occur at the leaf collar, at the culm and culm
+nodes and often at the panicle neck node.
+•	 Collar infection can kill the entire leaf blade.
+•	 Infection of the panicle neck node, called
+neck blast is the most destructive symptom.
+•	 The disease is seed-brone and seed
+transmitted.
+•	 Hence, the disease can occur from the
+seedling stage itself.</p>
+
+<h5>Pathogen Morphology</h5>
+<p>•	 Pyricularia grisea is the cause of Rice, Ragi
+and Pearl millet blast.
+•	 The name P. oryzae has been widely used in
+literature, but for the fungus attacking Rice, P.
+oryzae is morphologically indistinguishable
+from P. grisea which attacks Ragi, Pearl
+millet and other grasses.
+•	 The perfect state is an ascomycete,
+Magnaporthe grisea.
+•	 The mycelium is intercellular; the hyphae
+are septate, branched, uni- to multinucleate,
+hyaline to olivaceous.
+•	 Conidiophores are simple or rarely branched,
+2 - 4 septate, single or in fascicles emerging
+through stomata, olive to fuliginous, swollen
+at base, tapering towards lighter colored tip.
+•	 A single conidium develops at the tip of each
+conidiophore.
+•	 Conidia are pyriform (pear shaped) to
+obclavate, lightly pigmented, mostly 2
+septate, rarely 1 - 3 septate and measure
+19.2 - 27.3 x 8.7 - 10.3 µm, with a distinctly
+protruding basal hilum</p>
+<h5>Management</h5>
+<p>•	 Use resistant varieties.
+•	 Use disease free seed.
+•	 Treat the seeds with Carbendazim 50%
+WP (Bavistin/ GLSTIN) @ 4 gm/kg seed.
+•	 Give a fungicidal spray before planting.
+•	 Use balanced nutrition as high Nitrogen
+dose increases the susceptibility to
+blast.
+•	 Spray any of the following systemic fungicides
+2 - 3 times during the crop season.
+o Carbendazim 50% WP (Bavistin/
+GLSTIN) @1 gm/ltr. or
+o Tricyclazole 75% WP (Baan, Beam,
+Blastogan) @ 1 gm/ltr.
+</p>
+
+<h5>GUMMY STEM BLIGHT</h5>
+<p>Hosts: Bottle gourd, Ash gourd, Pumpkin, Musk melons, Cucumbers and Watermelons
+Causal organism: Didymella bryoniae
+Distribution: Temperate regions of the world. </p>
+<h5>Symptoms</h5>
+<p>•	 During the initial stage, the symptoms appear
+as pale brown or grey spots on leaves,
+petioles and stems.
+•	 Leaf spots are circular and grayish green
+changing to dark brown.
+•	 Diseased leaves are killed and small black
+spots (pycnidia) appear on affected leaves. 
+•	 Sap may exude from the nodal tissues and
+then dry to form lesions, which exude drops
+of resin coloured gum.
+•	 These lesions expand to the main stem
+which leads to wilting of plant.
+•	 The lesions may also appear as small,
+circular, dark green, water-soaked areas on
+fruits.</p>
 </Layout>
