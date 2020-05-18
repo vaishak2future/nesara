ffa80ab8f847ee507d66a6ecb551e9081d2c4389
Vaishak Kumar <vaishak.thimakapura@gmail.com>
2020-05-17 19:19:07 -0700

    added general symptoms and virus disease

--- src/pages/diseases.js
+++ src/pages/diseases.js
@@ -4,4 +4,447 @@ import tomato from "../../static/tomato.png"
 export default () => <Layout>
     <h3>Diseases</h3>
     <img src= {tomato}/>
-    </Layout>
+    <h4>Symptoms and Signs</h4>
+    <p>Symptoms are the manifestation of disease expressed as a result of pathogen activity in the host plant.
+     In some cases, pathogens produce structures like mycelium, spores, sclerotia, pycnidia, etc., on the surface of the infected parts.
+    These are called ‘Signs’, e.g., mildews, rusts, etc.
+    There are mainly three types of morpho- logical symptoms.
+Necrosis – Killing of plant tissues.
+Hypertrophy and hyperplasia – Over-
+development.
+Hypotrophy and hypoplasia – Under development and stunting.
+</p>
+
+<h4>Necrosis</h4>
+    <p>Necrosis means death of cells, tissues and organs due to pathogen activity.
+Most destructive type and damage is irreversible.Almost all types of pathogens (fungi, bacteria, viruses, etc.) cause necrosis. Following are different kind of necrotic symptoms.</p>
+<h4>Spots</h4>
+<p>The most common necrotic symptoms that occur on leaves, floral parts and fruits.</p>
+<p>They are well-defined localized areas of usually brown or tan necrotic tissue.</p>
+<p>These spots may be surrounded by margins of purple or some other dark color.</p>
+<p>Tiny spots are called ‘flecks’ or ‘specks’.</p>
+<p>When dark threads of a fungal pathogen are seen on the spots, it is called ‘blotch’.</p>
+<p>Sometimes fungal spots are raised and then they are called ‘tar spots’.</p>
+<p>‘Shot hole’ spots are those where the dead and collapsed tissue shrink and fall off leaving a hole inside a spot.</p>
+<p>In bacterial leaf spots they appear brown or water-soaked and usually remain localized</p>
+  
+<h4>Blight and Blast</h4>
+<p>Initially, browning of infected plant parts  occurs and then extensive death happens in 
+a rapid manner.</p>
+<p>The dead tissues soon disintegrate and give burnt or scorched appearances.</p>
+ <h4>Scorch</h4>
+ <p>Scorch resembles blight, but the scorch occurs in irregular patterns usually
+     along the margins of leaves, and, sometimes between the veins.
+ </p>
+ <h4>Scald</h4>
+ <p>Scald is the blanching of epidermal and adjacent tissues of fruit and, occasionally, of leaves.</p>
+<h4>Canker</h4>
+<p>A ‘Canker’ is really a symptom of an injury often associated with
+an open wound that has become infected by a fungal or bacterial pathogen.</p>
+<p>Canker diseases frequently kill branches or structurally weaken a plant until the infected area breaks free, often during strong winds.</p>
+<p>Citrus canker caused by the bacterium Xanthomonas axonopodis is one of the more common diseases.</p>
+<p>Infection causes lesions on the leaves, stems and fruit of citrus trees, including lime, oranges, and grapefruit.</p>
+
+<h4>Die-back</h4>
+<p>Severe necrosis of twigs or branches happens from the tip and advances downwards
+Citrus canker caused by the bacterium Xanthomonas axonopodis is one of the more common diseases.
+towards the base is called ‘die-back’ or ‘stag head’.</p>
+<p>The symptom often becomes apparent during the dormant season or shortly thereafter.</p>
+<p>Species of Colletotrichum (Chillies), Botryodiplodia (Mango), Phomopsis (Neem) and other fungi cause this symptom.</p>
+
+<h4>Anthracnose</h4>
+<p>This fungal disease affects many plants,
+including vegetables, fruits and trees.</p>
+<p>It causes dark, sunken lesions on leaves,
+stems, flowers and fruits.</p>
+<p>It also attacks developing shoots and
+expanding leaves.</p>
+<p>It can spread very quickly during rainy
+seasons.</p>
+<p>Anthracnose is a general term for a variety
+of diseases that affect plants in similar ways.</p>
+<p> On leaves, anthracnose generally appears
+first as small, irregular yellow or brown spots.</p>
+<p>These spots darken as they age and may also expand, covering the leaves.</p>
+<p>On vegetables, it can affect any part of the
+plant.</p>
+<p>On fruits, it produces small, dark, sunken
+spots, which may spread.</p>
+<p>In moist weather, pinkish spore masses form
+in the center of these spots.</p>
+<p>Eventually, the fruits will rot.</p>
+<p>On trees, it can kill the tips of young twigs.</p>
+<p>It also attacks the young leaves, which
+develop brown spots and patches.</p>
+<p>It can also cause defoliation of the tree.</p>
+
+<h4>Streak and Stripe</h4>
+<p>Streak represents elongated necrotic area
+along stems and leaf veins.</p>
+<p>Stripe is also elongated necrotic area but
+confined to leaf tissues between the veins,
+particularly in monocots.</p>
+
+<h4>Ring spots</h4>
+<p>Ring spots are the common type of symptoms
+produced by virus pathogens.</p>
+<p>They are characterized by the appearance
+of sometimes chlorotic but mostly necrotic
+rings on the leaves or also on the fruits and
+stems.</p>
+<p>The ring spots may be present singly or in
+groups developing concentrically on infected
+parts.</p>
+<p>Papaya ring spot, Tomato spotted wilt,
+Tobacco ring spot, etc., are common
+examples.</p>
+
+<h4>Rots</h4>
+<p>Rots represent dead and decomposed tissue
+on various parts of the host plant as a result of
+maceration and disintegration caused under
+the influence of various enzymes secreted by
+the pathogen.</p>
+<p>Though the rotting may result due to nonparasitic causal agencies, the great majority
+of rots are caused by fungal or bacterial
+pathogens.</p>
+<p>
+All parts are prone to rots and they are
+<ul>
+<li> Root rot / Rhizome rot – in Ginger,
+Turmeric, Chrysanthemum, etc.</li>
+<li>Stem rot of Rice caused by Leptosphaeria
+salvini.</li>
+<li>Bud rot – Coconut, Arecanut caused by
+Phytophthora palmivora.</li>
+<li>Fruit rot - Most of the vegetables have
+fungal fruit rots.</li>
+<li>Ear and seed rot- Various fungi cause ear
+rots in Maize.</li>
+<li>Leaf rot - Black rot of crucifers
+(Xanthomonas campestris pv. campestris)</li>
+<li>Tuber rot – Brown rot of Potato (Ralstonia
+solanacearum).</li></ul>
+</p>
+
+<h4>Scabs</h4>
+<p>Scabs are rough, crust-like localized lesions
+usually slightly raised or sunken and cracked
+giving a scabby appearance.</p>
+<p>Though they can occur on every part of the
+host but are frequently evident on fruits and
+tubers</p>
+
+<h4>Wilts</h4>
+<p>Wilt is usually a generalized secondary
+symptom where leaves and shoots lose their
+turgidity, become flaccid and finally droop
+(wilt).</p>
+<p>Normally this happens due to blocking of
+xylem vessels by the pathogen itself or by
+substances secreted by it.</p>
+<p>Wilts may be sudden without yellowing
+(bacterial wilt of solanaceous crops) or
+gradual, partial and slow wilt with yellowing
+of leaves and finally total wilt as in the case
+of Fusarium and Verticillium wilts.</p>
+<p>Plants also wilt because of rotting of roots
+and stems.</p>
+
+<h4>Rust pustules</h4>
+<p>These are the characteristic symptoms of
+rust fungi.</p>
+<p>All rust fungi are obligate pathogens.</p>
+<p>Rust pustules are normally erumpent;
+submerged, small or big in size.</p>
+<p>Generally rust pustules are surrounded by
+chlorotic halos.</p>
+<p>Though they are common on leaves, they
+can be found on stems also.</p>
+<p>Normal colour of the pustules is red or yellow,
+however, brown to dark colors are also not
+uncommon in fungi belonging to
+Basidiomycetes.</p>
+<p> Pustules may be of whitish colour in white
+rusts caused by Albugo candida under
+Oomycetes.</p>
+<p>And may be red in red rust caused by
+Cephaleurus parasitica, a green alga</p>
+
+<h4>Bleeding and Exudations</h4>
+<p>Refers to the abnormal secretion or oozing
+out of watery or slimy products from the
+affected plant parts, which may or may not
+set into hard masses.</p>
+<p>It is the major symptom of some fungal diseases.</p>
+<p>Citrus gummosis, Gummy stem blight in
+Cucurbits and stem bleeding in Coconut and
+Arecanut are some of the examples.</p>
+
+<h4>Damping-off</h4>
+<p>Sudden collapse of seedlings specially in
+nursery bed or tray.</p>
+<p> Fall over of the seedlings due to weakening of
+the base of the stem.</p>
+<p>Species of Pythium and Rhizoctonia are the
+common fungi that cause this.</p>
+<p> Excess moisture or dampness causes this
+disease.</p>
+
+<h4>Mildews</h4>
+<p>Mildews represent the chlorotic or necrotic
+areas generally covered with sporulation of
+the fungal pathogen.</p>
+<p>There are two types of mildews namely
+‘Downy’ and ‘Powdery’.</p>
+
+<h5>Downey Mildews</h5>
+<p>Downy mildews are caused by a group of
+Oomycetous ‘fungal’ pathogens belonging to
+family Peronosporaceae.</p>
+<p>Downy mildews occur on both monocots
+and dicots.</p>
+<p>Upper surface of the leaf show chlorotic
+spots (pale yellow/purplish / yellowish, etc.)
+of varying size and form which later merge
+into surrounding green tissues.</p>
+<p>The corresponding lower surface is covered with white cottony or downy growth, hence
+the name ‘Downy Mildew’.</p>
+<p>However, under severe disease condition, the
+cottony growth and sporulation can happen
+on upper surface also.</p>
+<p>In some cases the infection will occur on
+floral parts and even on fruits.</p>
+<p>In cereal Downy Mildews, phyllody of the
+inflorescence (green ear) is also caused.</p>
+
+<h5>Powdery Mildews</h5>
+<p>Powdery mildews are caused by fungi
+belonging to Ascomycetes and family
+Erysiphaceae.</p>
+<p>These are also obligate pathogens like Rust
+fungi.</p>
+<p>Powdery mildew is the most common disease
+occuring on a very large range of crops.</p>
+<p>The symptoms are typical and very easy to
+recognize owing to its powdery appearance.</p>
+<p>They produce chlorotic or necrotic areas
+covered superficially growing mycelia and
+fructifications of the pathogen.</p>
+<p>Black minute fruiting bodies are also visible
+at the end of the season.</p>
+
+<h4>Hyperplasia and Hypertrophy (Over Development)</h4>
+<p>Many pathogens, through their biochemical
+activity induce hormonal imbalance.</p>
+<p>This results in excessive growth of host
+tissues and causes abnormal increase in size
+of affected organs.</p>
+<p>It is brought about by one or both of the
+two processes known as hyperplasia and
+hypertrophy. </p>
+<p>Hyperplasia is the abnormal increase in the
+size of plant organs due to increase in number
+of cells by increased cell division rate.</p>
+<p>In hypertrophy the increased size of the
+organ is due to increase in the size of cells of
+a particular tissue.</p>
+
+<h4>Galls</h4>
+<p>Galls are localized enlargements on various
+plant parts in the form of small pustules or
+warts, larger tubercles, tumours or
+masses of cells making a morbid outgrowth
+of either fleshy or woody texture, in which
+host tissues and pathogens unite.</p>
+<p>Knots may be fleshy or woody in their
+texture.</p>
+<p>Nematode like Meloidogyne incognita, fungi
+like Protomyces macrosporus, bacteria like
+Agrobacterium tumefaciens are some of the
+common examples.</p>
+
+<h4>Warts</h4>
+<p>Warts are protuberances developed on
+tubers and stems generally giving warty or
+discolored Cauliflower-like appearance.</p>
+<p>Good example is wart disease of Potato
+caused by Synchytrium endobioticum. </p>
+
+<h4>Leaf Curl</h4>
+<p>These symptoms represent irregular and
+extensive wrinkling and furrowing of leaves
+due to reduced growth of veins in comparison
+with the growth of laminar tissue resulting
+in sunken veins and raised up laminar tissue
+leading to the curling of the leaves.</p>
+<p>Papaya, Tomato, Cotton, Tobacco, Chilli
+crops are most susceptible to leaf curl by
+whitefly transmitted viruses.</p>
+
+<h4>Hypotrophy and Hypoplasia</h4>
+<p>In some diseases, infected cells and tissues
+slowdown in development resulting in the
+infected plant parts thereof being arrested
+in their growth as a result of degeneration
+of the cells (hypotrophy) or subnormal cell
+divisions (hypoplasia).</p>
+<p>Though some of the fungal and bacterial
+diseases exhibit these symptoms, viruses
+produce more of these symptoms.</p>
+
+<h4>Chlorosis</h4>
+<p>Destruction of or inhibition of chlorophyll
+formation is called chlorosis.</p>
+<p>The leaves with less chlorophyll appear pale
+green or yellowish in color.</p>
+<p>Extreme case of chlorosis is ‘blanching’ or
+‘bleaching’</p>
+<p>Chlorosis is commonly caused by viruses.</p>
+<p> However, certain nutrient deficiencies also
+cause chlorosis.</p>
+<p>Variety of chlorotic symptoms are caused by
+viruses viz., mosaic, mottle, vein-chlorosis,
+vein-clearing, vein-banding, etc.</p>
+
+<h4>Mosaicing</h4>
+<p>Irregular intermingling of normal green with
+light green, yellow or white chlorotic areas
+imparting a mosaic-like pattern.</p>
+<p>Tomato mosaic, Tobacco mosaic, Pumpkin
+mosaic, Yellow vein mosaic of Okra, etc.</p>
+
+<h4>Leaf narrowing</h4>
+<p>The infected leaves generally become narrow
+due to reduced growth of laminar tissue,
+but the veins and midrib remain normal in
+growth.</p>
+<p>In extreme cases only the main veins remain
+normal while laminar tissue is almost
+completely absent, e.g. rat tail symptom in
+Tomato (CMV), Papaya (PRSV), etc.</p>
+
+<h4>Stunting or dwarfing</h4>
+<p>Refers to the reduction in size of either the
+whole plant or a specific plant part.</p>
+<p>It results in reduced size and loss of vigour.</p>
+<p>Stunting may be caused by viral, bacterial,
+fungal or nematode infections.</p>
+<p>Insect or mite feeding can also cause stuntingor dwarfing.</p>
+<p>Even abiotic factors like lack of water,
+imbalance of soil nutrients, excess light,
+chemical or mechanical injuries can cause
+stunting.</p>
+<p>Stunting due to abiotic factors can easily be
+corrected. </p>
+
+<h4>Rosetting</h4>
+<p>Rosetting means shortening of internodes of
+the infected plant along with the clumping of the foliage together at the apices of branches.</p>
+
+<h4>Fasciation</h4>
+<p>Fasciation also known as cresting, is a
+relatively rare condition of abnormal growth
+in vascular plants.</p>
+<p>Here the apical meristem (growing tip),
+which normally is concentrated around a
+single point and produces approximately
+cylindrical tissue, instead becomes elongated
+perpendicularly to the direction of growth,
+thus, producing flattened, ribbon-like, crested
+(cristate), or elaborately contorted tissue.</p>
+<p>Fasciation may also cause plant parts to increase in weight and volume in some
+instances.</p>
+<p>The phenomenon may occur in the stem,
+root, fruit or flower head.</p>
+<p> Some plants are grown and prized
+aesthetically for their development of
+fasciation.</p>
+<p> Any occurrence of fasciation has several
+possible causes, including hormonal, genetic,
+bacterial, fungal, viral and environmental
+causes.</p>
+
+<h4>Transformation or Replacement of Plant Organs</h4>
+<p>Normal plant organs particularly floral organs
+are replaced by a structure of the pathogen,
+causing severe yield losses.</p>
+<p>In case of smut, the ovary is transformed into
+structure containing smut spores.</p>
+<p>In ergot disease, the ovary is invaded by the
+fungus and transforms it into scelotial body.</p>
+<p>In Udbatta disease of Rice, which is also
+called false ergot, the seeds become pseudosclerotia.</p>
+
+<h5>Little leaf disease</h5>
+<p>This is a serious mycoplasma disease of
+Brinjal.</p>
+<p>The disease is transmitted by leaf hoppers
+(Cestius (Hishimonus) phycitis and Amrasca
+biguttula).</p>
+<p>The leaves of the infected plants in the early
+stages are light yellow in colour, reduced in
+size and are malformed.</p>
+<p>Disease affected plants are generally shorter
+in stature bearing a large number of branches,
+and leaves much much smaller than healthy
+plants. </p>
+<p>The petioles get shorter considerably,
+many buds appear in the axil of leaves
+and internodes get shortened thus giving
+the plants a bushy appearance (rosetting/
+witches broom symptoms).</p>
+<p> Flower parts are deformed leading the plants
+to be sterile.</p>
+<p>Infected plants do not bear any fruit.</p>
+<p>However, if any fruit is formed it becomes
+hard and tough and fails to mature.</p>
+<p>Recently this disease is also affecting Beans
+crop</p>
+
+<h5>Smuts</h5>
+<p>Smuts refer to sooty or charcoal-like powdery
+mass usually appearing on floral organs,
+particularly the ovary.</p>
+<p>However, they can occur on leaf, stem and
+roots.</p>
+<p>They are caused by Basidiomycetes belonging
+to order Ustilaginales.</p>
+<p>Smut infected tissues are either destroyed or
+replaced by black smut spores or they are first
+stimulated to divide and enlarge to produce
+a swelling or gall of varying size and then
+destroyed and replaced by smut spores.</p>
+<p>Corn, Sorghum, Wheat and Rice are the
+common smut hosts.</p>
+
+<h5>Ergots</h5>
+<p>The ergot causing fungus infects the florets
+and develops in the ovaries, producing initially
+copious creamy, pink, or red colored sweet
+sticky liquid called honeydew.</p>
+<p>The honeydew can drip down onto the
+upper leaves making them sticky.</p>
+<p>Often pollen and anther sacs adhere to the
+honeydew.</p>
+<p>Subsequently, long dark colored hard
+structures, sclerotia, develop from infected
+florets, first dark at the tip and then
+completely black.</p>
+<p>Udbatta disease or incense rod disease is
+also called ‘False Ergot of Rice’ as it is not
+Ergot in real sense.</p>
+
+<h5>Transformation of floral organs</h5>
+<p>Floral organs are transformed into leafy
+structures.</p>
+<p>Phyllody refers to the development of floral
+parts into leafy structures, generally caused
+by virus or Phytoplasma infection such as
+aster yellows.</p>
+<p>However, in case of Downy Mildew of Finger
+millet, Pearl millet and Maize (male tassel) the
+inflorescence can show phyllody.</p>
+<p>Phyllody causes the affected plant to become
+partially or entirely sterile.</p>
+</Layout>
---
+++ src/pages/diseases/virus-diseases.js
@@ -0,0 +1,35 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+export default () => <Layout>
+<h2>Diseases</h2>
+<h3>Virus Diseases</h3>
+<p>Viruses are small infectious agents that
+need the host for their survival, growth and
+reproduction.
+•	 They can infect all types of life forms, from
+animals and plants to microorganisms,
+including bacteria and archaea.
+•	 As pathogens, viruses are always intracellular,
+moving from cell to cell throughout the plant
+by way of plasmodesmata and vascular
+transport systems, most commonly the
+phloem.
+•	 While some viruses, such as Cucumber
+Mosaic virus, die quickly if outside a cell or if
+the cell dies, other viruses such as Tobacco
+Mosaic virus retain their ability to infect for
+years after the infected plant part dies.
+•	 Many different viruses can infect plants and
+disease spreads from infected plant to healthy
+plants by the following ways.
+o	 Mechanically by way of contaminated
+workers’ hands, tools and equipment.
+o	 Majority of the plant viral diseases are
+spread by insect vectors like Aphids,
+Thrips, Whitefly, Leafhopper, Mite or
+Nematodes.
+o	 And also through grafting.
+</p>
+
+</Layout>
