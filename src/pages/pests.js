7e00a6255ab2bb81eae46b1ee2de1bc597d3bf97
Vaishak Kumar <vaishak.thimakapura@gmail.com>
2020-05-18 10:01:02 -0700

    added pests page

---
+++ src/pages/pests.js
@@ -0,0 +1,497 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => <Layout>
+<h2>Pests</h2>
+<p>	 In layman terms, a pest is any undesirable
+organism that disturbs normal human life.
+•	 Scientific definition of a pest is any destructive
+organism, which damages cultivated plants,
+forests, stored products, domestic products
+etc., including aesthetic qualities.
+•	 All crops are attacked or affected by a
+variety of pests that include weeds, insects,
+nematodes, rodents, birds, diseases caused
+by fungi, bacteria, viruses, etc.
+•	 Despite using pesticides worth more than
+US$ 32 billion for crop protection worldwide,
+annually we incur crop losses to the tune of
+33 to 35 per cent. </p>
+
+<h4>INSECTS, MITES, NEMATODES AND THEIR MANAGEMENT</h4>
+<p>This chapter deals mainly with the damage caused by insects, mites and nematodes and their
+effective management.</p>
+
+<h4>Insects</h4>
+<p>•	 These include phytophagous insects that eat
+leaves, stems, flowers, fruits and roots of
+plants and those that also serve as vectors
+that carry disease causing bacteria, viruses
+and fungi.
+•	 They may be variously classified into
+taxonomic orders, based on venation of
+wings, mouth parts, feeding behaviour, types
+of injury they cause, etc.</p>
+
+<h5>Classification based on taxonomic orders</h5>
+<h6>Orthoptera</h6>
+<p>Orthoptera is an order of insects that
+comprises of grasshoppers, locusts and
+crickets.
+•	 The order is subdivided into two suborders:
+Chelicera – grasshoppers, locusts and
+close relatives; and Ensifera – crickets and
+close relatives.</p>
+
+<h6>Hemiptera</h6>
+<p>The Hemiptera or true bugs are an order of
+insects comprising some 50,000 to 80,000
+species of groups such as the whiteflies,
+cicadas, aphids, mealy bugs, scales, plant
+hoppers, leaf hoppers, and shield bugs.
+•	 They range in size from 1 mm to 15 cm, and
+share a common arrangement of sucking
+mouthparts</p>
+
+<h6>Lepidoptera</h6>
+<p>•	 Lepidoptera is an order of insects that
+includes butterflies and moths.
+•	 About 180,000 species of the Lepidoptera
+are described, in 126 families and 46 superfamilies, which is 10 per cent of the total
+described species of living organisms.
+•	 It is one of the most widespread and widely
+recognizable insect orders in the world.</p>
+
+<h6>Diptera</h6>
+<p>•	 Diptera is an order that includes the twowinged or so-called true flies e.g. fruit flies,
+shoot flies, leaf miners, etc.
+•	 It numbers more than 125,000 species that
+are relatively small, with soft bodies.
+•	 The mouthparts of flies are of the sucking
+type and many flies are of great economic
+importance</p>
+
+<h6>Thysanoptera</h6>
+<p>•	 Thrips are minute, slender insects with
+fringed wings and unique asymmetrical
+mouthparts.
+•	 Thrips species feed mostly on plants by
+puncturing and sucking up the contents,
+although a few are predators.
+•	 Approximately 6,000 species have been
+described.</p>
+
+<h6>Coleoptera</h6>
+<p>•	 Beetles and weevils are a group of insects
+that form the order Coleoptera, in the
+superorder Endopterygota.
+•	 Their front pair of wings is hardened into
+wing-cases (elytra), distinguishing them from
+most other insects.
+•	 This is the largest of all insect orders.</p>
+
+<h5>Classification based on  the mouth parts/feeding behavior/type of injury</h5>
+<p>•	 Insects cause injury to almost all parts of
+plants either directly or indirectly to secure
+food.
+•	 Mouthparts differ from insect to insect
+and the damage that they cause provides
+useful information for identification and
+classification of the pest.
+•	 Being able to identify the type of damage
+caused by the various types of mouthparts
+also helps to differentiate insect damage from
+mite damage and also to determine how to
+control the pest.
+•	 There are two main categories of mouthparts Biting and Chewing mouthparts and Nonchewing mouthparts.</p>
+
+<h5>I. BITING AND CHEWING MOUTHPARTS</h5>
+<p>Biting and Chewing mouthparts, often referred to as mandibulate mouthparts, are the most primitive
+type. </p>
+
+<h5>Chewing insects</h5>
+<p>•	 These insects chew, grind and swallow
+external part of the plant (leaves, stems,
+flowers, fruits, etc.) with the help of strong,
+well-developed mandibles.
+•	 These insects are phytophagous and belong
+to the orders Orthoptera (grasshoppers),
+Coleoptera (beetles), and Lepidoptera
+(caterpillars).
+•	 Some of these insects can eat to the extent
+of skeletonizing the leaves or even denuding
+the plants. e.g., Caterpillars, Ash weevil,
+Diamond back moth, etc.</p>
+
+<h5>Boring insects</h5>
+<p>•	 The insect larvae that feed by getting inside
+the plant tissue (stems, shoots, roots, fruits,
+pods, etc.) are called borers.
+•	 They make galleries as they feed upon the
+internal tissues, leave frass in them and make
+the plant weak.
+•	 Borers belong to insect orders Coleoptera,
+Lepidoptera and Diptera.
+•	 Borers of fruits are called worms (Lepidoptera
+– Cotton bollworms, Brinjal shoot & fruit
+borer, etc.).
+•	 Borers of seeds and grains are called weevils
+(Coleoptera – Rice weevil, Ash weevil,
+Coconut Rhinoceros beetle, etc.).
+•	 Borers of leaves are called leaf miners
+(Diptera – Leaf miners of Tomato, Bean,
+Cucurbits, etc.).</p>
+
+<h5>Damage</h5>
+<p>•	 Taking a closer look at the damage gives us
+a clue as to which kind of insect or mite has
+caused the damage.
+•	 The signs of damage that an insect or mite
+with piercing-sucking mouthparts has caused
+looks very different from the feeding pattern
+of an insect with chewing mouthparts.
+•	 Chewing mouthparts result in parts of the
+plant being (leaves, stems, flowers, fruits,
+roots and seeds) chewed or torn off, which
+can be easily identified by
+o Holes or notches on leaves
+o Surface feeder window
+o Stem chewed at base
+o Stem/shoot bored
+o Seed consumed
+o Fruit tunneled
+o Root damaged
+o Surface of tuber chewed or tunneled</p>
+
+<h5>Root eating subterranean insects</h5>
+<p>•	 Insects found in soil feed on the roots of
+plants and trees by chewing or boring or
+sucking the sap and forming galls.
+•	 Most soil inhabiting insects damage the
+crops in their larval stage and only a few
+spend their life cycle entirely in the soil (wire
+worms, cut worms, etc.).
+•	 Some insects live through several stages in
+the soil as in white grub of Finger millet and
+root grubs of Sugarcane, Arecanut, etc.
+•	 Though the damage caused to roots may
+vary depending on the species and crops
+affected, generally the affected plants show
+stunting, discoloration, withering and death
+of the plants</p>
+
+<h5>Gall Producers</h5>
+<p>•	 Gall-inducing insects cause external
+outgrowth or tumors of various shapes and
+sizes called galls.
+•	 The term gall fly is also used to describe these
+species, although most are not true flies.
+•	 These insects belong to the order Diptera
+(gall midges) or Hymenoptera (gall wasps).
+•	 Galls are formed by insect/mite feeding or
+egg-laying activity.
+•	 The adults initiate formation of galls
+by oviposition and the larvae continue
+development of galls.
+•	 Either mechanical damage or salivary
+secretions of insects and/or mites causes
+increased production of normal plant growth
+hormones, leading to localized increase in
+cell size and/or cell number.
+•	 Although the gall is entirely a plant tissue,
+the insects in some unknown manner control
+and direct the shape and size.
+•	 The outcome is an abnormal plant structure
+called a gall.
+•	 Due to the formation of galls, the growth of
+the plant may be impaired and yield may be
+adversely affected.</p>
+
+<h5>Injury by nest making</h5>
+<p>•	 Some insects remove parts of the plants for
+the construction of their nests though they
+do not feed on them.
+•	 In case of the red ant, the nest is constructed
+on the tree itself by webbing together a few
+leaves of Mango, Guava, Pomegranate, etc.</p>
+
+<h5>II NON-CHEWING MOUTHPARTS</h5>
+<p>Non-chewing mouthparts are modifications
+of mandibulate mouthparts and are used for ‘sucking’ up liquids or rasping across the
+surface of the leaf.</p>
+
+<h5>Piercing and sucking mouthparts</h5>
+<p>•	 Piercing and sucking mouthparts are
+modification of the mandibles and maxillae,
+which are elongated and modified to form
+a proboscis for piercing plant tissues.
+•	 The insect pierces the surface of the plant
+and sucks out the sap from within.
+•	 This action also removes the green
+chlorophyll from the plant.</p>
+<p>•	 While sucking, some insects inject saliva,
+excrete honey dew, etc.
+•	 Examples of insects with piercing & sucking
+mouthparts include, aphids, white flies,
+leafhoppers, mealy bugs, scales and fleas
+(Hemiptera).</p>
+
+<h5>Rasping and sucking mouthparts</h5>
+<p>•	 Rasping and sucking occurs when the insect
+or mite scrapes off the leaf surface and sucks
+up the fluids from the top layer of cells.
+•	 This causes the leaf to turn to a silvery color.
+•	 Examples of pests with rasping & sucking
+mouthparts include thrips (Thysanoptera)
+and mites. </p>
+<p>•	 The damage caused by thrips and mites
+looks very similar.
+•	 The dipteran flies shred the surface tissue to
+ingest the sap and bore tunnels (leaf miners
+and fruit flies).</p>
+
+<h5>Damage</h5>
+<p>•	 Insects with piercing & sucking mouthparts
+like aphids pierce parts of their host plant,
+causing the affected area to become brown
+or resulting in wilt or a tumor-like growth.
+•	 Pests with rasping & sucking mouthparts
+like thrips and mites remove the top layer of
+cells and suck out the cell contents, causing
+the leaf to look silvery or bronze in color. </p>
+<p>•	 In both the above types no part of the plant
+is missing.
+•	 Dipteran flies cause ragged wounds, mining
+on the surface of the leaves, stem, tunnel on
+the roots, bulbs, etc.
+•	 Most importantly these insects also
+transmit viruses.</p>
+
+<h5>Dissemination of plant diseases by insects</h5>
+<p>•	 Insects are responsible for spreading several
+plant diseases caused by bacteria, fungi and
+viruses.
+•	 While bacteria and fungi have alternative
+methods of dispersal, majority of plant
+viruses are dependent on insect vectors for
+their dissemination.
+•	 The insects act as mechanical carriers of the
+pathogens in or outside their body to pass them on to the plants by injection, contact or
+through contaminated ovipositor during egg
+laying.
+•	 Most of the insect vectors belong to the
+order Hemiptera (Aphids, White flies, Leaf
+hoppers and Mealy bugs).
+•	 A few belong to Thysanoptera (Thrips),
+Coleoptera (Beetles) and Orthoptera (Grass
+hoppers).</p>
+
+<h5>Mites</h5>
+<p>•	 Mites belong to the order Acarina of the
+class Arachnida.
+•	 The phytophagous mites belong mainly to
+the families Tetranychidae, Eriophyidae,
+Tarsonemidae and Tenupalpidae.
+•	 Mouth parts of mites are adapted for biting,
+piercing and sucking.</p>
+
+<h5>Diagnostic features of Tetranychidae and Eriophyidae</h5>
+<p>Tetranychidae | Eriophyidae
+Tiny and oval in shape.| Elongate and vermiform in shape.
+Body unsegmented and not divided into
+cephalothorax and abdomen.|
+Body distinctly divided into cephalothorax and
+abdomen.
+Possess 4 pairs of legs in the nymphal and
+adult stages and 3 pairs in the larvae.|
+Possess only 2 pairs of legs situated near the
+anterior end of the body, both in nymphal and
+adult stages.</p>
+
+<h5>Tetranychids</h5>
+<p>•	 Tetranychids penetrate the plant tissue with
+sharp stylets and remove the cell contents.
+•	 The chloroplasts disappear and the small
+amount of remaining cellular material
+coagulates to form an amber colored mass.
+•	 In the palisade layer, only the penetrated cells
+are damaged and continued feeding leads to
+irregular spots; transpiration rate accelerates,
+which finally leads to drying and dropping of
+leaves.
+•	 The mite infestation inhibits photosynthesis
+and changes the composition of leaf pigments
+leading to a complex of symptoms like
+o yellowing, bronzing, distortion, curling,
+crinkling, defoliation of leaves
+o retardation in growth.
+o premature dropping of flowers, reduction
+in size, quality and quantity of produce.</p>
+
+<h5>Eriophyids</h5>
+<p>•	 Eriophyids in general, cause no serious
+mechanical damage to plant tissue by their
+feeding.
+•	 Salivary growth regulators, when injected in
+to plants cause discolouration and growth
+modifications like galls, erinea blisters, rust,
+brooming, leaf edge rolling, etc.
+•	 The coconut mite Aceria guerreronis is
+an eriophyid mite, can destroy up to 90% of
+Coconut production.
+•	 It infests immature nuts and feeds on the
+portion of the nut covered by the perianth.</p>
+
+<h5>Tarsonemids</h5>
+<p>•	 Tarsonemids penetrate thin–walled and
+highly succulent tissues but are incapable of
+penetrating thick walled lignified tissues.
+•	 Occasionally toxins are injected, which
+presumably cause alterations in normal
+tissues.</p>
+
+<h5>Predatory Mites</h5>
+<p>•	 Predatory mites are the efficient natural
+enemies of phytophagous mites.
+•	 Mites of the family Phytoseiidae have
+been recognized as potential predators of
+phytophagous mites.
+•	 Phytoseiidae, Stigmaeidae, Anastidae,
+Cheyletidae and Erythradidae are the
+important predatory mite groups.
+•	 The Phytoseiid mites feed specially on mites
+of the families Tetranychidae, Eryophidae,
+Tarsoemidae and Tenupalpidae.
+•	 The miscellaneous groups include Bdellidae,
+Tarsonemidae and Tydeidae .</p>
+
+<h5>Mites known to transmit viral diseases</h5>
+<p>•	 Aceria tulipae is a vector of Wheat streak
+mosaic virus.
+•	 Aceria cajani transmits pigeon pea sterility
+mosaic virus.</p>
+
+<h4>Nematodes</h4>
+<p>•	 A number of genera and species of nematodes
+are known to cause significant damage to a
+great range of hosts, including foliage plants,
+agronomic and vegetable crops, fruit and
+nut trees, plantation crops, and commercial
+crops.
+•	 Some of the most damaging nematodes are:
+o	 Root knot (Meloidogyne spp.)
+o	 Cyst (Heterodera and Globodera spp.)
+o	 Root lesion (Pratylenchus spp.)
+o	 Spiral (Helicotylenchus spp.)
+o	 Bulb and stem (Ditylenchus dipsaci)
+o	 Reniform (Rotylenchulus reniformis)
+o	 Dagger (Xiphinema spp.)
+o	 Bud and leaf (Aphelenchoides spp.)</p>
+
+<h5>Morphology</h5>
+<p>•	 Nematodes are simple, multi-cellular animals
+typically containing 1,000 cells or less.
+•	 They are bilaterally symmetrical, softbodied, non-segmented and worm-like in
+appearance but are taxonomically distinct from earthworms, wireworms or flatworms.
+•	 The basic body plan of a nematode is a “tube
+within a tube.”
+•	 Most nematodes are not pathogens but
+rather saprophytes.
+•	 Some, however, are serious human, animal,
+and plant pathogens.
+•	 Those that attack animals or humans do not
+attack plants and vice versa.
+•	 Most nematode species that attack plants
+are microscopic and attack the roots, stem,
+foliage, and flowers of plants.
+•	 Plant-parasitic nematodes are typically
+microscopic, transparent, and vermiform;
+exceptions are the females of some genera,
+which become swollen and sac-like.
+•	 All plant parasitic nematodes have piercing
+mouthparts called stylets and the presence
+of a stylet is the key diagnostic sign
+differentiating plant parasitic nematodes
+from all other types of nematodes.</p>
+
+<h5>Signs and Symptoms</h5>
+<p>•	 Typical root symptoms indicating nematode
+attack are root knots or galls, root lesions,
+excessive root branching, injured root tips,
+and stunted root systems.
+•	 Symptoms on the above-ground plant parts
+are stunted growth, frequently chlorotic,
+wilting even with ample soil moisture, fewer
+and smaller leaves.
+•	 Root knot nematodes usually are detected
+first in localized areas within a field, nursery
+planting, or home garden, etc.
+•	 Affected field spots most often are circular to
+oval in shape and vary from a few feet across
+to several acres.
+•	 The damage is most conspicuous in the
+center of an infested area and diminishes
+toward the edges. Occasionally, entire fields
+are involved.
+•	 Crop yields can be reduced up to 90% or more,
+depending on the nematode population and
+biotic and abiotic factors.
+•	 Presence of root-knot nematodes cannot be
+diagnosed by above-ground symptoms alone,
+since factors such as poor drainage, low soil
+fertility, and other disease-causing organisms
+produce similar symptoms.
+•	 Bulb and stem nematodes produce swellings
+on stem and shortened internodes.
+•	 Bud and leaf nematodes distort and kill bud
+and leaf tissue.</p>
+
+<h5>Dissemination</h5>
+<p>•	 Parasitic nematodes are readily spread by any
+physical means that can move soil particles
+like equipment, tools, shoes, birds, insects,
+dust, wind and water. </p>
+
+<h5>Life cycle</h5>
+<p>•	 The life cycle of a typical plant-parasitic
+nematode is simple and direct.
+•	 An egg laid by the female develops into a
+first stage juvenile, which usually undergoes
+a first moult within the egg shell and hatches
+as a second-stage juvenile.
+•	 After hatching, the juvenile develops through
+two more stages; each completed by a moult,
+and becomes an adult after the fourth moult.
+•	 The time required to complete the life cycle
+varies considerably, from a week to several
+months, depending on the genus and on
+temperature and moisture.
+•	 Plant parasitic nematodes feed and reproduce
+on living plants and are capable of active
+migration in the rhizosphere, on aerial plant
+parts, and/or inside the plant.
+•	 Unlike free-living nematodes that are
+numerous in all soils, plant parasitic
+nematodes must feed on a plant host in
+order to complete their life cycle. •	 Root-knot nematodes are soil-borne and feed
+on roots.
+•	 Eggs hatch into juveniles that infect plant
+roots and take nutrients from the plant as
+they mature, causing characteristic knots or
+swellings.
+•	 Those that enter the root and develop
+into females are sedentary, become much
+enlarged, and lay hundreds of eggs in a sac
+on the root surface.
+•	 In moist soils above 270
+C, root-knot
+nematodes can go from egg to adult in about
+25 days.</p>
+
+<h5> Environmental Conditions</h5>
+<p>•	 Nematodes are most active in warm weather
+in moist, but well aerated, sandy soils in the
+presence of host plants.
+•	 They are most abundant in the upper foot of
+soils, but will follow roots several feet deep.</p>
+</Layout>
