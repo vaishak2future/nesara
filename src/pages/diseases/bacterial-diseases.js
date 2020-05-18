e08ace47db95db1b11e2e2c0135ad2eec416fd20
vaishak2future <vaishak.thimakapura@gmail.com>
2020-05-17 20:18:12 -0700

    finished pasting text to viral diseases and started bacterial diseases

---
+++ src/pages/diseases/bacterial-diseases.js
@@ -0,0 +1,194 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+export default () => <Layout>
+<h2>Diseases</h2>
+<h3>Bacterial Diseases</h3>
+<p>Bacteria are prokaryotic microorganisms that
+are typically a few micrometres in length.
+•	 These single celled microorganisms are
+usually found in masses or chains of several
+individuals.
+•	 Bacteria are found in most habitats be it soil,
+water, acidic hot springs, deep down the
+earth’s crust and even on radioactive waste.
+•	 While a great majority of bacteria are obligate
+saprophytes, many become parasitic and
+cause diseases of humans, animals and
+plants.
+•	 More than 80 species of bacteria and their
+numerous subspecies, also called pathovars
+(pv), are known to cause diseases in plants.
+•	 However, about half of all known plant
+pathogenic bacteria belong to the genus
+Pseudomonas, about one third to the genus
+Xanthomonas and about an eighth to the
+genus Erwinia.
+•	 While Bacteria appear in several shapes
+such as spherical, ellipsoidal, rod-shaped,
+spiral, comma-shaped or filamentous,
+interestingly, almost all the plant pathogenic
+bacteria are rod-shaped, only exception
+being Streptomyces, which is filamentous.</p>
+
+<p>Most plant pathogenic bacteria are flagellate.
+•	 The rod-shaped bacterial cells may be
+monotrichous (single flagellum at one end),
+lophotrichous (a tuft of flagella at one end), amphitrichous (a single or tuft of flagella
+at both ends), or peritrichous (flagella
+distributed all over the surface).</p>
+
+<p>Almost all plant pathogenic bacteria grow
+parasitically in the host plant and partly in
+debris or in the soil as saprophytes.
+•	 Agrobacterium tumefaciens (crown gall),
+Pseudomonas solanacearum (bacterial wilt)
+and Streptomyces scabies (common scab of
+Potato) have the ability to survive indefinitely
+in soil as saprophytes in the absence of their
+hosts (soil inhabitants).
+•	 But, most plant pathogenic bacteria have
+poor ability to survive in soil for long in
+the absence of their respective hosts (soil
+invaders).
+•	 They reproduce asexually mainly by binary
+fission once in 20 - 30 minutes producing
+one million cells in ten hours provided the
+conditions are favourable.
+•	 Bacterial diseases in crop plants affect stems,
+leaves, fruits, roots, or are carried internally
+without external symptoms.
+•	 They can cause a variety of symptoms
+including spots, blights, wilts, cankers, galls,
+scabs, etc.
+•	 These symptoms can be extremely noticeable
+and characteristic, but can also be difficult to
+spot and diagnose.
+•	 Most of the bacterial diseases are caused
+by different species of two major genera
+Pseudomonas (Ralstonia) and Xanthomonas.
+•	 However, some others like cankers, rots,
+galls, and scabs are caused by species of
+Clavibacter, Erwinia, Clostridium, Bacillus,
+Agrobacterium, Streptomyces, etc.</p>
+
+<h5>Bacterial Spots</h5>
+<p>Leaf spots are the most common of bacterial
+diseases.
+•	 Spots appear on leaves, blossoms, stems
+and fruits.
+•	 Spots on leaves of dicot plants are initially
+confined between the leaf veins and will
+appear angular and water-soaked.
+•	 In monocots, symptoms appear as streaks or
+stripes. </p>
+<p>Spots may coalesce resulting in large areas
+of necrotic tissue often surrounded by a
+chlorotic halo.
+•	 In some cases bacterial ooze, which is a
+characteristic feature for diagnosis of bacterial
+infections is also seen.
+•	 Almost all bacterial leaf spots and blights
+are caused by the members of genera
+Pseudomonas and Xanthomonas</p>
+
+<h5>BACTERIAL BLIGHTS
+</h5>
+<p>Symptoms vary depending on the crop and
+the plant part affected.
+•	 Bacterial blight of Rice and Pomegranate are
+among the more serious bacterial blights.
+•	 In Rice, symptoms appear on the leaves
+as pale-green to grey-green, water-soaked
+streaks near the tip and margins.
+•	 These lesions coalesce and become yellowishwhite with wavy edges. </p>
+<p>The whole leaf may eventually be affected,
+which become whitish or greyish and die.
+•	 In Pomegranate, one to many small watersoaked, dark colored, irregular spots appear
+on leaves.
+•	 Premature defoliation occurs under severe
+conditions.
+•	 The pathogen also infects stem, fruits and
+branches causing girdling and cracking
+symptoms.</p>
+
+<h5>BACTERIAL VASCULAR WILTS</h5>
+<p>Vascular wilts caused by bacteria primarily
+affect herbaceous vegetable crops, field
+crops, ornamentals and some tropical plants.
+•	 The pathogen enters, multiplies and moves
+through the xylem vessels of the host plant
+and interferes with the translocation of
+nutrients and water by producing gum.
+•	 Wilting starts with the lower leaves, and within 2-3 days, the whole plant wilts and
+collapses.
+•	 The wilt is almost sudden and the leaves do
+not exhibit any yellowing as seen in wilts
+caused by fungi like Fusarium or Verticillium.
+•	 When the crown region is cut open, vascular
+browning is seen and it yields bacterial ooze
+when kept in a test tube containing clear
+water.</p>
+<p>Ralstonia solanacearum causes the wilt in
+Solanaceous crops like Tomato, Tobacco,
+Eggplant, Hot & Sweet Peppers, Potato and
+Ginger, Banana, etc.
+•	 Erwinia tracheiphila causes wilt in cucurbits
+like Zuchini, Cucumber, Melons, etc.
+•	 Cucurbit wilts are transmitted by striped and
+spotted cucumber beetles.
+•	 Stewart’s wilt of Maize caused by Erwinia
+stewartii is transmitted by flea beetles.
+•	 It is a seed-borne disease as well.</p>
+
+<h5>BACTERIAL SOFT ROTS</h5>
+<p>The soft rots causing bacteria include Erwinia
+spp., Pseudomonas spp., Bacillus spp. and
+Clostridium spp.
+•	 Many soft rots are caused by nonphytopathogenic saprophytic bacteria.
+•	 Soft rots affect a large number of hosts
+causing damage to fleshy parts of the plants
+both in the standing crop and during storage. </p>
+<p>The bacteria enter through wounds and
+produce enzymes that break down the middle
+lamella causing separation of the cells, death
+and degeneration at the site of the infection.
+•	 The infected tissues become watery, soft and
+slimy and foul smelling.</p>
+
+<h5>LITTLE LEAF DISEASE</h5>
+<p>This is a serious disease of Brinjal caused by
+Phytoplasma.
+•	 Recently it is found to infect Beans also. </p>
+<p>The disease is transmitted by leaf hoppers
+(Cestius (Hishimonus) phycitis and Amrasca
+biguttula).
+</p>
+
+<h5>Symptoms</h5>
+<p>Disease affected plants are generally shorter
+in stature and bear more number of branches
+and leaves smaller than healthy plants.
+•	 The leaves turn light yellow and are smaller
+in size and malformed.
+•	 The petioles get shorter considerably,
+many buds appear in the axil of leaves andinternodes get shortened thus giving the
+plants a bushy rosetted appearance.
+•	 Flower parts are deformed leading to sterility.
+•	 Infected plants seldom produce fruits and if
+any fruit is formed, it becomes hard and fails
+to mature.</p>
+<h5>Management</h5>
+<p>Uproot and burn infected plants immediately
+on noticing the symptoms.
+•	 Use barrier crops or trap crops.
+•	 Adopt sanitary measures including
+eradication of susceptible volunteer crop
+plants. Schedule sowing time to avoid the main
+flights of the beet leaf hopper.
+•	 Spray systemic insecticides starting with the
+appearance of the leaf hoppers to control
+their population.
+•	 Power Plus has proved to control this
+disease also.</p>
+</Layout>
