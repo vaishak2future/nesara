6c56712f308d7dd56a392b17e6216d442cb9730d
Vaishak Kumar <vaishak.thimakapura@gmail.com>
2020-05-18 14:49:05 -0700

    began seed-borne diseases

---
+++ src/pages/diseases/seed-borne-diseases.js
@@ -0,0 +1,114 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+export default () => <Layout>
+<h2>Diseases</h2>
+<h3>Seed-borne Diseases</h3>
+<p>•	 The diseases that are of the seeds and
+carried through the seeds are called Seedborne diseases.
+•	 Seed-borne diseases are caused by
+Oomycetes, fungi, bacteria, viruses and
+nematodes.
+•	 All true/vegetative seeds are infected by the
+above pathogens.
+•	 The seed primordium or the maturing seed
+may be infected either
+(a) Directly from the infected plant through
+the flower or fruit stalk and the seed stalk
+or directly from the seed surface, or
+(b) Infection from outside may be introduced
+through stigma or ovary wall or pericarp,
+and the flower or fruit stalk, and later
+through the seed coat.
+•	 A pathogen may penetrate several of these
+parts of the seed (seed coat, endosperm and
+embryo) and in turn infect them.
+•	 The infestation/contamination of the seed
+may occur during harvesting, threshing,
+processing and storage.
+•	 The pathogen may, thus, be carried with the
+seeds in three ways.
+(a) Admixture: Pathogens are independent
+of seeds but accompany them. e.g.,
+Sclerotia of Ergot get mixed with healthy
+seeds during threshing.
+(b) External: Pathogen may be present on
+seed surface as spores, oospores and
+chlamydospores as in case of Karnal
+bunt of Wheat, covered smut of Barley,
+downy mildew of Pearl millet, etc.
+(c) Internal: Pathogens establish within
+the seed with definite relationship with
+seed parts.
+•	 Seed-borne diseases/pathogens can spread
+from the seed and infect the new plant in
+several ways.
+•	 Upon sowing, moisture activates pathogens
+causing pre and post emergence dampingoff.
+•	 Some of the pathogens like different
+species of Fusarium, Pythium, Rhizoctonia,
+Sclerotinia, Alternaria also cause similar
+diseases in several other crops.
+•	 All seed transmitted diseases are seedborne, but all seed-borne diseases are not
+seed transmitted.</p>
+
+<h5>Damage Caused</h5>
+<p><h5>1. Disease transmission</h5>
+•	 Seed-borne pathogens transmit diseases
+between fields, regions and countries
+through seed and other planting material.
+e.g., diseases like Bacterial blight of Rice,
+Sclerotinia diseases of Broad Beans,
+Common Beans and recently Cauliflower are
+transmitted through movement of improved
+seed.
+•	 Importance of transmission can be realized
+when we know the seeding rate (kg/ha),
+percentage seed infection and number
+of infected seeds/kg of seed for e.g., in
+case of Loose Smut of Wheat and Barley,
+0.1% seed infection brings in 5000
+infected seeds/hectare of field, which give
+rise to equal number of infected plants
+(systemic) and in such cases yield losses
+are expected in the same ratio of 1:1.
+<h5>2. Complete loss or reduction in seed
+germination</h5>
+•	 Macrophomina phaseolina infection in Bean
+seeds causes 59% loss of germination.
+•	 Cercospora kikuchii infection in Soybean
+causes 12% loss of germination.
+•	 Seed-borne smut fungi in a number of cereals
+and Pigeonpea Sterility Mosaic Virus cause
+severe seed abortion resulting in 80 to 100%
+yield losses.
+<h5>3. Reduction in seed quality</h5>
+•	 Infections by pathogen often substantially
+reduce seed size and weight reduction, e.g.,
+Leaf blight of Sunflower caused by Alternaria
+helianthi, A. zinnniae causing up to 80%
+yield loss.
+•	 Species of Aspergillus, Fusarium, Diplodia,
+Botryodiplodia, Nigrospora, Tinfect standing
+Maize causing seed rot.
+•	 Sclerotisation, stromatisation and gall
+formation e.g., Claviceps fusiformis causing
+stromatisation of millet seeds leading to 60-
+70% yield loss, Anguina tritici causing seed
+galls in Wheat.
+•	 Seed discoloration is a very important and
+wide spread symptom produced on seed
+indicating presence of pathogen, resulting in
+reduction in market value e.g., Cercospora
+kikuchii in Soy bean, Fusarium moniliformae
+in Sorghum, Aschochyta pisi in sweet pea.
+•	 Infected seeds may also be contami-nated
+with mycotoxins and nutritional changes.
+•	 Biochemical changes in seed products. e.g.,
+Groundnuts infected with A. flavus gives
+inferior quality of oil through reduction of
+the refractive index.
+<h5>4. Reduction in yield:</h5> Great yield losses are
+experienced worldwide through seed-borne
+pathogens.</p>
+</Layout>
