14f18e628bda0cc001e6c3f91bcc6530a64c0e0e
Vaishak Kumar <vaishak.thimakapura@gmail.com>
2020-05-18 15:06:48 -0700

    done pasting into seed-borne

--- src/pages/diseases/seed-borne-diseases.js
+++ src/pages/diseases/seed-borne-diseases.js
@@ -111,4 +111,287 @@ the refractive index.
 <h5>4. Reduction in yield:</h5> Great yield losses are
 experienced worldwide through seed-borne
 pathogens.</p>
+
+<h5>Common Seed-borne Diseases of Major Crops</h5>
+<p>Crop, Disease, Pathogen</p>
+<p>Wheat Loose Smut Ustilago tritici
+Karnal Bunt Neovassia indica
+Black point Alternaria tenuis
+Hill Bunt Tilletia caries
+Helminthosporium leaf blight Helminthosporium sativum
+Yellow ear rot (Bacterial) Corynebacterium michiganense pv. tritici
+Ear Cockle (Nematode) Anguina tritici
+Rice Brown spot Helminthosporium oryzae
+Bakanae Fusarium moniliforme
+Bacterial blight Xanthomonas campestris pv. oryzae
+Bunt Neovossia horrida
+Stack burn Alternaria padwickii
+Blast Pyricularia oryzae
+White tip (Nematode) Aphelenchoides besseyi
+Maize Southern corn blight Helminthosporium maydis
+Smut Ustilago maydis
+Ear rot pathogens Cephalosporium acremonium
+Fusarium graminearum, F. moniliforme
+Diplodia maydis
+Botryodiplodia theobromae
+Trichoderma viride
+Trichothecium roseum
+Nigrospora oryzae
+Storage fungi Aspergillus flavus, A. niger, A. ruber, A. candidus, etc.
+Sorghum Grain smut or Kernel smut Spacelotheca sorghi
+Head Smut S. reiliana
+Sugary disease Sphacelia sorghi
+Grain molds Curvularia lunata, Fusarium moniliforme, F. semitectum,
+Phoma sorghina, etc.
+Pearl millet Grain smut Tolyposporium penicillariae
+Green Ear (Downy Mildew) Sclerospora graminicola
+Ergot Claviceps microcephala
+Cotton Anthracnose Colletotrichum gossypii
+Black Arm Xanthomonas campestris pv. malvacearum
+Cabbage Black leg Phoma lingam
+Alternaria leaf blight Alternaria brassicae, A. brassicicola
+Black rot Xanthomonas campestris pv. campestris
+Pulses Anthracnose Colletotrichum spp.
+Ascochyta blight (Chickpea) Ascochyta rabiei
+Ashy stem blight (Cowpea) Macrophomina phaseolina</p>
+<p>Purple stain (Soybean) Cercospora kikuchii
+Collar rot (Groundnut) Aspergillus flavus
+Root rot (Groundnut) Macrophomina phaseolina
+Fusarium wilt (Chickpea) Fusarium oxysporum f. sp. ciceri
+Bacterial blight (Pea) Pseudomonas syringae pv. pisi
+Bacterial blight (Soybean) Xanthomonas phaseoli pv. sojensis
+Peanut mottle Peanut mottle virus
+Soybean mosaic Soya virus 1
+Tomato Early blight Alternaria solani
+Bacterial spot Xanthomonas campestris pv. vesicatoria
+Bacterial canker Clavibacter michiganenssis sub sp. michiganensis
+Bacterial wilt Pseudomonas solanacearum
+Bacterial Pith necrosis Pseudomonas corrugata
+Bacterial speck Pseudomonas syringae pv. Tomato
+Mosaic Tobacco Mosaic Virus
+Brinjal Phomopsis blight Phomopsis vexans
+Chillies Anthracnose Colletotrichum sps.
+Bacterial spot Xanthomonas campestris pv. vesicatoria
+Mosaic Tobacco Mosaic Virus, Pepper Mild Mosaic Virus
+Cucurbits Anthracnose Colletotrichum orbiculare
+Gummy stem blight Didymella bryoniae
+Bacterial fruit blotch of Watermelon Acidovorax avenae subsp. citrulli
+Bacterial leaf spot Xanthomonas campestris pv. cucurbitae
+Squash Mosaic Squash Mosaic Virus</p>
+
+<h5>Management of seed-borne diseases</h5>
+<p>•	 Take up seed production in diseasefree areas.
+o	 Choose dry areas with low humidity and
+irrigation facilities.
+o	 Schedule the time of planting to avoid
+disease incidence.
+o	 Isolate seed crop from fields containing
+possibly diseased plants.
+•	 Good production practices.
+o	 Use certified seed.
+o	 Minimize plant stress - fertilization &
+irrigation.
+o	 Manage weeds effectively.
+o	 Select well drained soils.
+o	 Follow a seed rate that ensures proper
+plant density to promote rapid drying of
+foliage.
+o	 Destroy/ plough under the crop residues.
+o	 Practise proper crop handling (wash
+hands & implements).
+o	 Remove infected plants (rouging).
+o	 Avoid working in field when wet.
+•	 Eradicate disease-causing pathogen
+from production area.
+o	 Remove alternate hosts and volunteer
+host plants.
+o	 Rotate crop and follow sanitation - residue
+management.
+o	 Create conditions unfavorable to
+pathogens.
+o	 Adopt seed treatment.
+o	 Use resistant/ tolerant crop varieties.
+o	 Use disease-free planting materials.
+o	 Spray protective fungicides.
+o	 Control Insect vectors.
+•	 Isolation and Field Inspection.
+o	 Seed crops should be isolated from all
+sources of pollen contamination and
+seed-borne diseases.
+o	 Crop should be inspected at least once at
+appropriate stage of growth.
+o	 At least 20% of the crops of certified seed
+should be inspected.
+o	 Presence of any seed-borne disease
+should be at the lowest possible level.</p>
+
+<h5>Seed Treatment</h5>
+<p>•	 Seed treatment refers to the application
+of fungicide, insecticide, or a combination
+of both, to seeds so as to disinfect and
+disinfest them from seed-borne or soil-borne
+pathogenic organisms and storage insects.
+•	 It also refers to the exposing seeds to solar
+energy, immersing in conditioned water, etc.
+Benefits of Seed Treatment
+•	 Prevents spread of plant diseases that are
+seed-borne and seed transmitted.
+•	 Prevents seed rot and seedling blights.
+•	 Improves seed germination.
+•	 Provides protection from storage insects and
+sucking insects at early stages.
+•	 Prevents damage by soil inhabiting insects.
+Types of Seed Treatment
+1) Seed disinfection
+•	 Seed disinfection refers to the eradication
+of fungal spores that have become
+established within the seed coat, or deeper
+tissues.
+•	 For effective control, the fungicide must
+actually penetrate the seed in order to kill
+the fungus.
+2) Seed disinfestation
+•	 Seed disinfestation refers to the destruction
+of surface-borne organisms that have
+contaminated the seed surface but not
+infected the seed surface.
+•	 Chemical dips, soaks, fungicides applied
+as dust, slurry or liquid have been found
+successful.
+3) Seed protection
+•	The purpose of seed protection is to protect
+the seed and young seedling from organisms
+in the soil, which might otherwise cause
+decay of the seed before germination.
+Conditions under which seeds must be
+treated
+1) Injured Seeds
+•	 Seeds suffer mechanical injury during
+combining and threshing operations, or
+from being dropped from excessive heights.
+•	 They may also be injured by weather or
+improper storage.
+•	 Any break in the seed coat affords an
+excellent opportunity for the pathogen to
+enter the seed and either kill it, or weaken
+the seedling that will grow from it.
+2) Diseased seed
+•	 Seeds may get infected by disease causing
+organisms even at the time of harvest, during
+processing, if processed on contaminated
+machinery or if stored in contaminated
+containers or warehouses.
+3) Undesirable soil conditions
+•	 Seeds are sometimes planted under
+unfavorable soil conditions such as cold and
+damp soils or extremely dry soils.
+•	 Such conditions could be favorable to the
+growth and development of certain fungi
+enabling them to attack and damage the
+seeds.
+4) Disease-free seed
+•	 Seeds are invariably infected by disease
+causing organisms, ranging from those
+having no economic consequence to those
+causing severe economic consequences.
+•	 Seed treatment provides insurance against
+seed transmitted diseases, soil-borne
+organisms and thus affords protection to
+weak seeds enabling them to germinate in to
+healthy seedlings.
+</p>
+<h5>Precautions in Seed Treatment:</h5>
+<p>•	 Most products used in the treatment of seeds
+are harmful to humans.
+•	 Extreme care is required to ensure that
+treated seeds are never used as human or
+animal food.
+•	 Treated seeds should be clearly labeled as
+being dangerous, if consumed.
+•	 The possibilities of unsold treated seeds
+being used for human or animal feed can
+be reduced, if care is taken to treat only the
+quantities that are expected to be sold.
+•	 Care must also be taken to treat seeds with
+correct dosage of the formulation. Applying
+in excess or too little can be as damaging as
+not treating at all.
+•	 Seeds with higher moisture content are more
+susceptible to injury when treated with some
+of the concentrated liquid products.
+•	 If seeds are to be treated with bacterial
+cultures, the sequence of treatment should
+be,
+i. Chemical treatments
+ii. Insecticide and fungicide treatments
+iii. Special treatments</p>
+
+<h5>Fungicides used in seed treatment</h5>
+<p>Thiram, Captan Commonly used broad spectrum contact fungicides.
+Carbendazim (Bavistin), Thiophanate
+Methyl (Roko/Topmast)
+Broad spectrum systemic fungicides against white colored
+fungi like Fusarium, Verticillium, Colletotrichum, Phomopsis,
+Pyricularia, Cercospora, Phoma, etc.
+Metalaxyl (Apron 35 SD) Systemic fungicide specific to downy mildews, Pythium and
+Phytophthora.
+Carboxin (37.5%) + Thiram (37.5%)
+(Vitavax Power)
+Fungicide specific to Smuts, Potato scurf, and Rhizoctonia
+diseases.</p>
+
+<h5>Insecticides used in seed treatment</h5>
+<p>Imidacloprid 48% WW (Gaucho)
+Provides protection against highly damaging sucking pests
+and virus spread due to these vectors, upto 30 to 40 days
+after sowing.
+Chlorpyriphos (Altima) Insecticide used against all insects and nematodes (before
+sowing or planting).
+Power Plus or Phyton-T
+(Greenlife)
+Special formulations for better germination, root growth
+and vigor. Compatible with all other products.</p>
+
+<p>Treatment of Ginger Rhizome
+• Treat Ginger rhizome with a solution of
+Metalxyl 35 WS (1 gm/ltr.) + Carbendazim
+or Thiophanate Methyl (1 gm/ltr.) +
+Chlorpyriphos (2 ml/ltr.) + Power Plus or
+Phyton-T (5 ml/ltr.) for 30 minutes.
+• Dry the rhizome under shade and store in
+cool place till they sprout.
+Treatment of Potato against black scurf
+Dipping tubers:
+• Dissolve 200 gm Vitavax Power in 100 ltr of
+water. Dip for 15-20 minutes and dry the
+tubers in shade before planting.
+For spraying on tubers:
+• Dissolve 200 gm Vitavax Power in 5 ltr water.
+Spray uniformly on the tubers and dry under
+shade before planting.
+Seed treatment with bio-agents:
+• Seeds can also be treated with bio-control
+agents like Trichoderma viride, T. harzianum,
+T. koningii, T. hamatum, Pseudomonas
+fluorescens etc., to protect them against
+seed-borne and soil-borne diseases.
+Dosage:
+Vegetable seeds:
+• Mix 5 to 10 gm of formulation in 1 kg seed.
+Field crops:
+• Mix 100 to 500 gm of formulation with the
+quantity of seeds required for one acre.
+Sugarcane and tuber crops (Onion, Potato,
+etc.):
+• Mix 2.0 kg of formulation with seed material
+required for one acre.
+Note: Follow the recommendation of the
+manufacturers of the products.
+Method of application:
+•	 Make a paste or slurry by mixing 5 gm of the
+formulation in 1 litre of water or Rice gruel.
+•	 Pour 1kg of seed on to the paste or slurry and
+mix thoroughly to coat the seeds uniformly.
+•	 Shade dry the coated seeds for 20-30
+minutes before sowing.</p>
 </Layout>
