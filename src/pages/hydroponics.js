9d32475a6db5208f6c3e74e47d66c691a0a9e49b
vaishak2future <vaishak.thimakapura@gmail.com>
2020-06-21 20:56:34 -0700

    added hydroponics and site home

---
+++ src/pages/hydroponics.js
@@ -0,0 +1,71 @@
+import React from "react"
+import Layout from "../components/layout"
+import logo from "../../static/survey.jpg"
+import CourseCard from "../components/courseCard"
+import soil from "../../static/soil.jpg"
+import water from "../../static/water.jpeg"
+import tomato from "../../static/tomato.png"
+import def from "../../static/nutrition/leaf.jpg"
+import { css } from "@emotion/core"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default () => 
+
+    <Layout>
+        <h1>Hydroponics</h1>
+        <p>Hydroponics is a method of cultivating plants in a soil-free medium, and supplying them with the mixture of nutrients required through water. In place of soil, a sterile and inert growing medium is used, and combined with nutrient enriched water. </p>
+
+        <p>Quite surprisingly, hydroponics is not a modern technology at all! Although hydroponics is considered high-tech, it is as old as the pyramids. </p>
+
+<p>The first hydroponic garden existed probably around 600 BC. Plants have been growing in lakes and seas from ages. Humans probably tried imitating this in the ancient city of Babylon. The earliest successful experiments of hydroponics were in the  Hanging Gardens of Babylon. Hieroglyphic records found on the banks of the Nile, dating back several hundred years, describe cultivation of plants without soil using only water. </p>
+
+<p>Theophrastus (327-287 BC) conducted various experiments in crop nutrition. Botanical studies of Dioscorides date back to the first century A.D. </p>
+
+<p>The Aztecs, a nomadic tribe of Central America, were driven to the marshy shore of Lake Tenochtitlan (Mexico) in the 11th century. Without having fertile lands to grow their crops, they came up with an ingenious method of cultivation. They made rafts, called Chinampas, from rushes and reeds, tying the stalks with roots. They then put the soil from the shallow button of the lake, which was rich in organic debris, on these rafts and grew plants on them. The plants were well nourished by the soil and the roots grew through the soil, through the raft, into the lake water below. The Spaniard invaders documented these structures in the 16th century AD.</p> 
+
+<p>The excitement around hydroponics was always prevalent through the ages. But hydroponic technology became economically viable only in the late 1970s with the advent of hi-tech plastics and simpler system design coming into being. The energy saving poly greenhouse covers, the PVC (or similar) pipe used in the feed systems, the nutrient injector pumps and reservoir tanks are all made of types of plastic that weren't available prior to the 1970s.
+In hydroponic cultivation, the plants are provided with the particular minerals and nutrients they require. So, unlike in soil, plants don’t have to continuously grow their root system in search of water, nutrients and oxygen. Instead the energy is spent on production of leaves, flowers and fruits. Therefore, hydroponic plants will develop flowers and fruits with optimum levels of appearance, yield and flavour. Hydroponics allows growers to get 2 to 10 times more yield from their plants, in just half the space and half the time.</p>
+
+<p>It is possible to monitor pH and nutrients that plants are getting and to see that they get the exact nutrients they need in hydroponic cultivation. The hydroponic systems are closed and they recycle the water that is not used by plants. The ability to grow indoors allows farmers to control temperatures and lighting schedules to improve plant production. Systems can be designed to make use of vertical space and increase planting density. Hydroponics also allow us to create farms in locations where soil conditions are too poor to support farming, or space is limited and a farm otherwise couldn’t exist.</p>
+
+<p></p>Any plant can grow hydroponically, but some will do better than others. Hydroponic cultivation is ideal for crops such as peppers, cucumbers and tomatoes, leafy crops, like lettuce and herbs and flowering plants. The other crops that can be grown are strawberries, spring onion, blueberries, coriander, potatoes and green beans.
+
+<p></p>Plants that need more horizontal space to grow are impractical to grow in this type of greenhouse. Watermelon and other melons, squash, and corn are some of these vegetables. Plants that need bees to pollinate them are also labour intensive to be grown hydroponically. Flowers differ in their requirements. So, it is advisable to research each type before starting their cultivation hydroponically.
+Commercial hydroponic farmers complement  hydroponic technology with a controlled environment to achieve the highest quality produce. Inside a green- house the ambient temperature, humidity and lux can be controlled allowing year-round cultivation.
+
+<p>Hydroponics is mostly practiced in countries such as Japan, USA, India, South Africa, Australia and UK.
+As the technology gets better, hydroponics may even be productive enough, to feed people around the world. Other areas where hydroponics could be used in the future include growing seedlings for reforestation, establishing orchards, growing ornamental crops, flowers and shrubs and integration with aqua-culture, where the wastes provides nutrients to the plants and the plants help to purify the water the fish are living in.</p>
+
+<p>Hydroponics have the following advantages over traditional farming methods:</p>
+
+<p>
+<ul>
+<li>Water use efficiency is very high and upto 90% water can be saved in this method.</li>
+<li>Productivity increases 2 to 10 folds in the same amount of space.</li>
+<li>If the hydroponic system is managed well, many crops can be produced twice as fast as in conventional farming.</li>
+<li>Indoor farming in a climate controlled environment means farms can exist in places where weather and soil conditions are not favorable for traditional food production.</li>
+<li>The need for chemical weed or pest control products does not arise when operating a hydroponic system. When problems are kept at bay, even labour requiring activities become less.</li>
+<li>Hydroponics has a reduced footprint.</li>
+<li>Reduced disease and pests in hydroponics</li>
+<li>The media used can be reused and therefore is environment friendly.</li>
+<li>Although the initial cost of setting up is more, the returns are high over time.</li>
+<li>It is convenient as it is adaptable for small spaces </li>
+<li>Hygienic as it is easy to sterilise the components of the system.</li>
+</ul></p>
+
+
+<p> The challenges in Hydroponic cultivation are:
+<ul>
+<li>It needs adequate knowledge and commitment to pursue.</li>
+<li>Algae formation in the system.</li>
+<li>Clogging due to fertilizer residue</li>
+<li>Water and electricity risks due to close proximity.</li>
+<li>High installment costs.</li>
+<li>Unexpected power outages.</li>
+<li>Quick spread of diseases and pests if not prevented.</li>
+<li>Uncertain ROI.</li>
+</ul></p>
+
+<p>These challenges can be overcome with planning and experience. Hydroponics is here to stay. The market for hydroponics is expected to expand dramatically in the coming years. It is valued at USD 23.94 billion (2018) and is forecast to account for a CAGR of 6.8% from 2020 to 2025 according to  Research and Markets.</p>
+
+    </Layout>
