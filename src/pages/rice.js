dabc2172b1c48301d94f2e0db5bbaa6b483cd546
Manthan Dhingra <manthan.dhingra@gmail.com>
2020-07-10 21:16:45 -0400

    in page navigation added to rice, tomato, and hydroponics pages. In page navigation background and padding added. Hydroponics card added to home screen with pic

--- src/pages/rice.js
+++ src/pages/rice.js
@@ -17,9 +17,27 @@ export default () =>
         </head>
 
         <body>
+       
         <h1>Rice</h1>
-            <p>Rice, family name Oryza, is a globally consumed grain, largely consisting of carbohydrates, rice is essential to many cuisines around the world. As a grain, it is inexpensive, widely available, easy to cook, good-tasting, and substantial, making it the perfect food for many people. 
-</p>
+           
+          
+            <p>Rice, family name Oryza, is a globally consumed grain, largely consisting of carbohydrates, rice is essential to many cuisines around the world. As a grain, it is inexpensive, widely available, easy to cook, good-tasting, and substantial, making it the perfect food for many people. </p>
+    <div style={{
+        backgroundColor: 'beige',
+        width: '40%',
+        paddingLeft: '2%',
+        paddingTop: '1%',
+        paddingBottom: '1%'
+    }}>
+        <div><a href="#anchorVarieties">Varieties</a></div>
+        <div><a href="#anchorEconomics">Economics</a></div>
+        <div><a href="#anchorOptimalConditions">Optimal Conditions</a></div>
+        <div><a href="#anchorPlanting">Planting</a></div>
+        <div><a href="#anchorHarvest">Harvest</a></div>
+        <div><a href="#anchorFertilizationSoilCare">Fertilization and Soil Care</a></div>
+        <div><a href="#anchorOtherInfo">Other Info</a></div>
+    </div>
+            <h2 id="anchorVarieties"></h2>
             <h4>Varieties</h4>
                 <p>
                   There are numerous rice varieties, most of which are relatively similar in taste and nutrition. They are often classified into groups based on their length. They are divided into: <strong>Short-grain, Medium-grain, and Long-grain.</strong>  
@@ -29,21 +47,22 @@ export default () =>
                       <li>Short grain varieties include: Sushi rice, bomba rice, etc.</li>
                   </ul>
                 </p>
+            <h2 id="anchorEconomics"></h2>
             <h4>Economics</h4>
-                <p>Rice is primarily produced in Southern and Central Asia, where it is also primarily consumed. The top five largest producers of rice are China, India, Indonesia, Bangladesh, and Vietnam, from most tons of rice produced to least. These nations produce a combined yield of over 500 million tons of rice annually. Worldwide, rice is the third most consumed grain, just behind corn and wheat.
-</p>
+                <p>Rice is primarily produced in Southern and Central Asia, where it is also primarily consumed. The top five largest producers of rice are China, India, Indonesia, Bangladesh, and Vietnam, from most tons of rice produced to least. These nations produce a combined yield of over 500 million tons of rice annually. Worldwide, rice is the third most consumed grain, just behind corn and wheat.</p>
+                <h2 id="anchorOptimalConditions"></h2>
             <h4>Optimal Conditions</h4>
-                <p>The rice plant is highly dependent on water, and is usually grown in fields flooded with water to ensure that water is constantly available to the plant. Generally, these plants thrive in warm environments with plenty of sunlight, with the temperature reaching at least 70 degrees Fahrenheit or about 21 degrees Celsius. The soil should be slightly acidic, and preferably be able to retain water, like clay. Your region is especially important, because the rice plant needs heat for a sustained period of time, lasting up to six months.
-</p>
+                <p>The rice plant is highly dependent on water, and is usually grown in fields flooded with water to ensure that water is constantly available to the plant. Generally, these plants thrive in warm environments with plenty of sunlight, with the temperature reaching at least 70 degrees Fahrenheit or about 21 degrees Celsius. The soil should be slightly acidic, and preferably be able to retain water, like clay. Your region is especially important, because the rice plant needs heat for a sustained period of time, lasting up to six months.</p>
+                <h2 id="anchorPlanting"></h2>
             <h4>Planting</h4>
-                <p>Growing and planting rice may seem intimidating at first, however it is relatively simple. First, obtain rice seeds. Rice from the grocery store will not work, as most varieties sold in stores are heavily processed. To begin planting, soak seeds in water for a day to break the seeds’ dormant state. Then, either choose to plant directly into the ground or consider growing seedlings to later transplant. Transplantation is more labor intensive, however, it yields crops with a lesser need for weed control, and is a popular method of rice cultivation in Asian countries. To prepare the ground for plantation, clear out all weeds by plucking them from the root, and till the soil, ensuring that the soil is somewhat consistant and mixed evenly. Planting in a bucket, pot, or other small container may be the better choice for individuals seeking to try out growing rice for the first time. This allows you to have a higher degree of control in the amount of water the plants receive. If you intend to plant rice on a large scale, ensure that you have created a field with rows to grow rice, and sunken rows to accommodate water to flood the field. These divots should span the entire row of crops, and hold between 2 and 9 inches of water, depending on the size, variety, and location of the plants. After 2-3 weeks, fertilize the rice crops, and they will continue to grow. They take 3-6 months to reach maturity, depending on weather and variety of rice.
-</p>
+                <p>Growing and planting rice may seem intimidating at first, however it is relatively simple. First, obtain rice seeds. Rice from the grocery store will not work, as most varieties sold in stores are heavily processed. To begin planting, soak seeds in water for a day to break the seeds’ dormant state. Then, either choose to plant directly into the ground or consider growing seedlings to later transplant. Transplantation is more labor intensive, however, it yields crops with a lesser need for weed control, and is a popular method of rice cultivation in Asian countries. To prepare the ground for plantation, clear out all weeds by plucking them from the root, and till the soil, ensuring that the soil is somewhat consistant and mixed evenly. Planting in a bucket, pot, or other small container may be the better choice for individuals seeking to try out growing rice for the first time. This allows you to have a higher degree of control in the amount of water the plants receive. If you intend to plant rice on a large scale, ensure that you have created a field with rows to grow rice, and sunken rows to accommodate water to flood the field. These divots should span the entire row of crops, and hold between 2 and 9 inches of water, depending on the size, variety, and location of the plants. After 2-3 weeks, fertilize the rice crops, and they will continue to grow. They take 3-6 months to reach maturity, depending on weather and variety of rice.</p>
+                <h2 id="anchorHarvest"></h2>
             <h4>Harvest</h4>
-                <p>The harvest process for the rice plant is time consuming and labor intensive. First, the field must be drained of the floods, and allowed to dry somewhat. Then, the plants are to be cut by hand or machine, and allowed to dry. Following the drying of the plants, the rice is repeatedly slammed upon a surface, freeing whole grains from the plant, and allowing them to seep through perforations. Then, the unprocessed grains are once again dried, and can be roasted. Following this process, the grains are milled down to remove the hull, and depending on the type of rice desired, some grains are processed down until the bran and germ is removed, creating white rice. 
-</p>
+                <p>The harvest process for the rice plant is time consuming and labor intensive. First, the field must be drained of the floods, and allowed to dry somewhat. Then, the plants are to be cut by hand or machine, and allowed to dry. Following the drying of the plants, the rice is repeatedly slammed upon a surface, freeing whole grains from the plant, and allowing them to seep through perforations. Then, the unprocessed grains are once again dried, and can be roasted. Following this process, the grains are milled down to remove the hull, and depending on the type of rice desired, some grains are processed down until the bran and germ is removed, creating white rice. </p>
+            <h2 id="anchorFertilizationSoilCare"></h2>
             <h4>Fertilization and Soil Care</h4>
-                <p>In maintaining soil and ensuring that crops receive the correct nutrients, fertilization is essential. The rice crop has been shown to benefit from the use of nitrogen and phosphorus fertilizer. Nitrogen is the nutrient that is required by the crop in the highest amounts.
-</p>
+                <p>In maintaining soil and ensuring that crops receive the correct nutrients, fertilization is essential. The rice crop has been shown to benefit from the use of nitrogen and phosphorus fertilizer. Nitrogen is the nutrient that is required by the crop in the highest amounts.</p>
+                <h2 id="anchorOtherInfo"></h2>
             <h4>Other info</h4>
                 <h6>Brown Rice vs White Rice</h6>
                     <p>Brown rice and white rice are the same product, just processed to different stages. Brown rice is the less refined of the two, having been milled to remove the outer layer of each grain of rice. The bran and germ is milled off of each grain of brown rice, and produces white rice.</p>
--- src/pages/tomato.js
+++ src/pages/tomato.js
@@ -19,14 +19,33 @@ export default () =>
         
         </head>
         <body>
+        
         <h1>Tomato</h1>
             <div>
             <p>The tomato plant is an easy to grow and relatively forgiving plant. They are best grown in warm, sunny conditions, and have little tolerance for frost. <b>Here are some tips to help you start growing your own tomato plants</b></p>
             </div>
+
+    <div style={{
+        backgroundColor: 'beige',
+        width: '40%',
+        paddingLeft: '2%',
+        paddingBottom: '1%',
+        paddingTop: '1%'
+    }}>
+      
+    
+      <div><a href="#anchorVarieties">Varieties</a></div>
+      <div><a href="#anchorPlanting">Planting</a></div>
+      <div><a href="#anchorCareAndMaintenance">Care and Maintenance</a></div>
+      <div><a href="#anchorOtherInfo">Other Information</a></div>
+
+    </div>
+            <h2 id="anchorVarieties"></h2>
             <h4>Varieties</h4>
             <h6>Bush (determinate) vs Vine (indeterminate)</h6>
             <p>There are two main types of tomato plant: <strong>Bush Tomatoes</strong> (a.k.a. determinate) and <strong>Vining Tomatoes</strong> (a.k.a. indeterminate). Bush tomatoes are the more simple to grow out of the two varieties, and require less maintenance. They generally grow anywhere from 2 feet to 5 feet, based on the variety. They can be grown easily in garden beds and do not require the use of stakes and support systems. They remain in-season for a shorter period than vining varieties and yield most of their tomatoes around the same time as opposed to a staggered fashion. Vining tomato plants require more maintenance than bush tomatoes, however they are also said to yield a more desirable, flavorful product. If they are provided with the correct supports and nutrition, vining tomato varieties generally grow to around 5-6 feet, but have been known to exceed that height. Their yield is gradual, and provides tomatoes at a relatively constant rate throughout the season. </p>  
             <p></p>
+            <h2 id="anchorPlanting"></h2>
             <h4>Planting</h4>
             <h6>When To Plant:</h6>
             <p>Tomatoes grow best in warm weather, and weather is largely dependent on geographic location, so different regions have different planting windows, usually lasting multiple weeks. To decide when to plant in your region, research local weather ahead of time to determine the date of the last frost, and consider planting seeds indoors, 6-8 weeks before the last frost date. This will allow you to get a head-start on the growth of your plant. Optimal temperature for fruit production is between 65 and 85 degrees Fahrenheit, or between 15 and 30 degrees Celsius. </p> 
@@ -42,6 +61,7 @@ export default () =>
                     <li>The soil does not consist of heavy clay (unsuitable for tomatoes)</li>
                     </ul>
                     <div>Before planting, create stakes or purchase a tomato plant support, like a cage or a stake, and plant the seedling along the support, so it can be tied down after some growth.</div></p>
+            <h2 id="anchorCareAndMaintenance"></h2>
             <h4>Care and Maintenance</h4>
             <h6>Watering:</h6>
                 <p>Water regularly, every three to four days once mature, more if the weather is dry or hot. In the watering of tomatoes, it is recommended to water the roots directly instead of the plant. Watering is crucial, so do not rush. Ensure that the watering is slow and reaches deeply, preferably through a method of drip irrigation. The application of mulch to the top level of soil may help preserve water and slow the evaporation process, which benefits the plant.  </p>
@@ -57,7 +77,8 @@ export default () =>
                         <p>Make sure that when fertilizing, all raw fertilizer is mixed and dispersed evenly throughout the soil and not in direct contact with the root in concentrated amounts to prevent harm from the </p>
                 </div>
             <h6>Support Systems:</h6>
-                <p>When growing many varieties of tomatoes, support structures such as stakes and cages are essential to the growth of the plant and to the stability of the plant. Stakes can be inserted either prior to the plantation or afterwards, and can be tied to the plant's stem and branches as it begins to grow taller. Similarly, cages also support the plant's stem and branches, but do not need to be tied.</p>
+                <p>When growing many varieties of tomatoes, support structures such as stakes and cages are essential to the growth of the plant and to the stability of the plant. Stakes can be inserted either prior to the plantation or afterwards, and can be tied to the plant's stem and branches as it begins to grow taller. Similarly, cages also support the plant's stem and branches, but do not need to be tied.</p>                <h2 id="anchorVarieties"></h2>
+            <h2 id="anchorOtherInfo"></h2>
             <h4>Other Important Info</h4>
             <h6>Nutritional Info</h6>
             <p>Here is the nutritional information for tomatoes (per 100g)
